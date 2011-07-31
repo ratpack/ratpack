40a7c6b7741b90095507d53ca9fe0aa5442d1c16
pstehlik <philip.stehlik@gmail.com>
2011-07-31 10:28:54 -0700

    adding IDEA plugin modifying runner/app/servlet to have a central place for the pre-processing of the app-script

--- src/main/groovy/com/bleedingwolf/ratpack/RatpackRunner.groovy
+++ src/main/groovy/com/bleedingwolf/ratpack/RatpackRunner.groovy
@@ -1,29 +1,11 @@
 package com.bleedingwolf.ratpack;
 
-import groovy.lang.Binding
-import groovy.util.GroovyScriptEngine
-import com.bleedingwolf.ratpack.RatpackServlet
-import com.bleedingwolf.ratpack.RatpackApp
 
 class RatpackRunner {
 	RatpackApp app = new RatpackApp()
-	
+
 	void run(File scriptFile) {
-		GroovyScriptEngine gse = new GroovyScriptEngine(parentDirectoryName(scriptFile))
-
-		Binding binding = new Binding()
-		binding.setVariable('get', app.get)
-		binding.setVariable('post', app.post)
-		binding.setVariable('put', app.put)
-		binding.setVariable('delete', app.delete)
-		binding.setVariable('set', app.set)
-
-		gse.run scriptFile.name, binding
-
+    app.prepareScriptForExecutionOnApp(scriptFile)
 		RatpackServlet.serve(app)
-	}
-	
-	private String parentDirectoryName(File scriptFile) {
-		scriptFile.canonicalPath.replace(scriptFile.name,'')
 	}
 }
