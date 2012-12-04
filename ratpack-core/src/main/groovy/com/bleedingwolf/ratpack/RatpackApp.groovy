367ee0742e6a008631299c2f5d34d0280c5351aa
Luke Daley <ld@ldaley.com>
2012-12-04 00:02:23 -0500

    fix static file handling.

--- ratpack-core/src/main/groovy/com/bleedingwolf/ratpack/RatpackApp.groovy
+++ ratpack-core/src/main/groovy/com/bleedingwolf/ratpack/RatpackApp.groovy
@@ -5,6 +5,12 @@ import org.mortbay.jetty.Server
 import org.mortbay.jetty.servlet.Context
 import org.mortbay.jetty.servlet.ServletHolder
 import com.bleedingwolf.ratpack.internal.RatpackServlet
+import org.mortbay.jetty.handler.ResourceHandler
+import org.mortbay.jetty.handler.HandlerList
+import org.mortbay.jetty.servlet.ServletHandler
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import org.mortbay.resource.Resource
 
 class RatpackApp {
 
@@ -31,9 +37,28 @@ class RatpackApp {
 
     server = new Server(port)
     def context = new Context(server, "/", Context.SESSIONS)
-    context.setResourceBase(staticFiles.absolutePath)
+    def handlerList = new HandlerList()
+
+    def resourceHandler = new ResourceHandler() {
+      @Override
+      void handle(String target, HttpServletRequest request, HttpServletResponse response, int dispatch) {
+        Resource resource = getResource(request)
+        if (resource == null || !resource.equals(getBaseResource())) {
+          super.handle(target, request, response, dispatch)
+        }
+      }
+    }
+    resourceHandler.resourceBase = staticFiles.absolutePath
+    resourceHandler.welcomeFiles = []
+    handlerList.addHandler(resourceHandler)
+
     def servlet = new RatpackServlet(router, renderer)
-    context.addServlet(new ServletHolder(servlet), "/*")
+    def servletHandler = new ServletHandler()
+    servletHandler.addServletWithMapping(new ServletHolder(servlet), "/*")
+    handlerList.addHandler(servletHandler)
+
+    context.setHandler(handlerList)
+
     server.start()
   }
 
