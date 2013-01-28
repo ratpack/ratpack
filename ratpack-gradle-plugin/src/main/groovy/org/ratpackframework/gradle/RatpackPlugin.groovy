9a7f36a0b47ddb770810e533b83c2dbc32fa434c
Luke Daley <ld@ldaley.com>
2013-01-28 21:06:44 +0000

    Fix artifact group and add snapshot repo.

--- ratpack-gradle-plugin/src/main/groovy/org/ratpackframework/gradle/RatpackPlugin.groovy
+++ ratpack-gradle-plugin/src/main/groovy/org/ratpackframework/gradle/RatpackPlugin.groovy
@@ -55,7 +55,7 @@ class RatpackPlugin implements Plugin<Project> {
 
     project.dependencies {
       runtime 'org.slf4j:slf4j-simple:1.6.3'
-      compile "org.ratpackframework:ratpack-core:${version}"
+      compile "org.ratpack-framework:ratpack-core:${version}"
     }
 
     def configureRun = project.task("configureRun")
