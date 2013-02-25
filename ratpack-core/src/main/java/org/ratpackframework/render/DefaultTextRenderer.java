dd88f762e1c7aa6d880c5c3dff96357243fcfa12
Luke Daley <ld@ldaley.com>
2013-02-25 12:15:02 +0000

    javadoc

--- ratpack-core/src/main/java/org/ratpackframework/render/DefaultTextRenderer.java
+++ ratpack-core/src/main/java/org/ratpackframework/render/DefaultTextRenderer.java
@@ -1,5 +1,8 @@
 package org.ratpackframework.render;
 
+/**
+ * Converts the object to a string by simply calling its {@code toString()} method.
+ */
 public class DefaultTextRenderer implements TextRenderer {
 
   @Override
--- ratpack-core/src/main/java/org/ratpackframework/render/TextRenderer.java
+++ ratpack-core/src/main/java/org/ratpackframework/render/TextRenderer.java
@@ -1,5 +1,8 @@
 package org.ratpackframework.render;
 
+/**
+ * Converts the given object to a text representation.
+ */
 public interface TextRenderer {
 
   String render(Object object);
