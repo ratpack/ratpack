eca0ff5984102dbd69a0d6a0348f8a69316f4a13
robfletcher <robert.w.fletcher@gmail.com>
2013-04-19 09:14:58 +0100

    copied template project

---
+++ src/ratpack/ratpack.groovy
@@ -0,0 +1,8 @@
+import org.ratpackframework.app.*
+import org.ratpackframework.groovy.app.Routing
+
+(this as Routing).with {
+	get('/') { Request request, Response response ->
+		response.redirect('index.html')
+	}
+}
