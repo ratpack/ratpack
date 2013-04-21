224473b5a467acec985d8eb241cf6c3ea6743797
Luke Daley <ld@ldaley.com>
2013-04-21 12:58:33 +0100

    Imports.

--- ratpack-core/src/main/java/org/ratpackframework/app/Endpoint.java
+++ ratpack-core/src/main/java/org/ratpackframework/app/Endpoint.java
@@ -1,8 +1,5 @@
 package org.ratpackframework.app;
 
-import org.ratpackframework.app.Request;
-import org.ratpackframework.app.Response;
-
 public interface Endpoint {
 
   void respond(Request request, Response response);
