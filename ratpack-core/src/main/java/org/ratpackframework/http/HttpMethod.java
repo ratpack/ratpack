a4f74acb37ed935688e59b4e4cec52190241c7c7
Luke Daley <ld@ldaley.com>
2013-05-14 15:37:40 +0100

    Some javadoc.

--- ratpack-core/src/main/java/org/ratpackframework/http/HttpMethod.java
+++ ratpack-core/src/main/java/org/ratpackframework/http/HttpMethod.java
@@ -16,18 +16,52 @@
 
 package org.ratpackframework.http;
 
+/**
+ * The method of a HTTP request.
+ */
 public interface HttpMethod {
 
+  /**
+   * The name of the method, always in upper case.
+   *
+   * @return The name of the method, always in upper case.
+   */
   String getName();
 
+  /**
+   * True if the method is POST.
+   *
+   * @return True if the method is POST.
+   */
   boolean isPost();
 
+  /**
+   * True if the method is GET.
+   *
+   * @return True if the method is GET.
+   */
   boolean isGet();
 
+  /**
+   * True if the method is PUT.
+   *
+   * @return True if the method is PUT.
+   */
   boolean isPut();
 
+  /**
+   * True if the method is DELETE.
+   *
+   * @return True if the method is DELETE.
+   */
   boolean isDelete();
 
+  /**
+   * Returns true if the method has the given name, insensitive to case.
+   *
+   * @param name The name of the method to compare to the actual method name.
+   * @return True if the given name equals {@link #getName()} irrespective of case.
+   */
   boolean name(String name);
 
 }
