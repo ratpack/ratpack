9b1424babf083d113d1398d46c466509d9e20117
Luke Daley <ld@ldaley.com>
2013-05-20 20:57:16 +0100

    Some work on the website content.`

--- ratpack-site/src/ratpack/ratpack.groovy
+++ ratpack-site/src/ratpack/ratpack.groovy
@@ -3,5 +3,11 @@ import static org.ratpackframework.groovy.RatpackScript.ratpack
 ratpack {
   handlers {
     assets "public", "index.html"
+    path("forums") {
+      get("user") {
+        // TODO update this to the vanity url
+        response.redirect "http://ratpack-users.19683.n7.nabble.com/"
+      }
+    }
   }
 }
