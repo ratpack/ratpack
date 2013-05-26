744dfe12938d2ba17d14f88f237972df435b8d76
Luke Daley <ld@ldaley.com>
2013-05-26 17:04:50 +0100

    No need to create a module just to instantiate a handler.

--- ratpack-guice/src/main/java/org/ratpackframework/guice/internal/InjectingHandler.java
+++ ratpack-guice/src/main/java/org/ratpackframework/guice/internal/InjectingHandler.java
@@ -16,7 +16,6 @@
 
 package org.ratpackframework.guice.internal;
 
-import com.google.inject.AbstractModule;
 import com.google.inject.Injector;
 import org.ratpackframework.handling.Exchange;
 import org.ratpackframework.handling.Handler;
@@ -24,27 +23,14 @@ import org.ratpackframework.handling.Handler;
 public class InjectingHandler implements Handler {
 
   private final Class<? extends Handler> handlerType;
-  private final AbstractModule module;
 
   public InjectingHandler(final Class<? extends Handler> handlerType) {
     this.handlerType = handlerType;
-    module = new AbstractModule() {
-      @Override
-      protected void configure() {
-        bind(handlerType);
-      }
-    };
   }
 
   public void handle(Exchange exchange) {
-    // TODO what's the cost of creating an injector for every request
-    //      if it's not negligible, we could support using an explicit injector instead of getting
-    //      it from the request context.
-    //
-    //      Another option would be to cache the injector. If the injection context is the same, then just reuse.
     Injector injector = exchange.get(Injector.class);
-    Injector childInjector = injector.createChildInjector(module);
-    Handler instance = childInjector.getInstance(handlerType);
+    Handler instance = injector.getInstance(handlerType);
     instance.handle(exchange);
   }
 
