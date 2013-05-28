93d635817800f2dac1c55c121bc5281f2eb6e2c2
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-05-28 10:37:23 +0100

    get Ratpack to serve IE magic header so HTML validator doesn't barf

--- ratpack-site/src/ratpack/ratpack.groovy
+++ ratpack-site/src/ratpack/ratpack.groovy
@@ -2,6 +2,12 @@ import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 ratpack {
   handlers {
+  	handler {
+      if (request.path.empty || request.path == "index.html") {
+        response.addHeader "X-UA-Compatible", "IE=edge,chrome=1"
+      }
+      next()
+    }
     assets "public", "index.html"
   }
 }
