90b8d4dbb996b3a86f812e679273e319fe8dca24
Luke Daley <ld@ldaley.com>
2013-05-30 11:43:19 +0100

    Some style improvements to the Javadoc syntax highlighting.

--- ratpack-manual/src/api-assets/highlight.js
+++ ratpack-manual/src/api-assets/highlight.js
@@ -2,7 +2,7 @@ var pres = document.getElementsByTagName("pre");
 for (var i = 0; i < pres.length; i++) {
   var pre = pres[i];
   pre.className = pre.className + " language-groovy";
-  pre.innerHTML = "<code>" + pre.innerHTML + "</code>";
+  pre.innerHTML = "<code>" + pre.innerHTML.replace(/^\s+|\s+$/g, '') + "</code>";
   Prism.highlightElement(pre, false, null);
 }
 
