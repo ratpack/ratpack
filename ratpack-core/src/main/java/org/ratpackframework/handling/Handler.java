777f0b5bb76f3fb5709137a92e92bf55e9c5642e
Luke Daley <ld@ldaley.com>
2013-05-30 10:49:32 +0100

    Some work on the handler javadoc.

--- ratpack-core/src/main/java/org/ratpackframework/handling/Handler.java
+++ ratpack-core/src/main/java/org/ratpackframework/handling/Handler.java
@@ -29,6 +29,13 @@ import org.ratpackframework.api.NonBlocking;
  * Handlers are fundamentally asynchronous. This means that there is no guarantee that the handler is “finished” when its {@link #handle(Exchange)}
  * method returns. This means that handlers <b>must</b> ensure to do <i>something</i> with the response. Where <i>something</i> is either send
  * a response or delegate to another handler.
+ * <h3>Handler chains</h3>
+ * <p>
+ * Handlers are always implicitly connected in a chain like structure. The {@link Exchange} that the handler operates on provides
+ * the {@link Exchange#next()} method that allows the next handler in the chain to process the exchange.
+ * The last handler in the chain is always the “404” handler in that it returns an empty body 404 response to the client.
+ * <p>
+ * The {@link Chain} type can be used to “build” handler chains.
  * <h3>Types of handlers</h3>
  * <p>
  * Handlers do not necessarily generate a response to the request, though they can.
@@ -38,31 +45,32 @@ import org.ratpackframework.api.NonBlocking;
  * <p>
  * <ol>
  * <li>Send a response back to the client, terminating processing</li>
- * <li>Pass control to the next handler in the pipeline via {@link org.ratpackframework.handling.Exchange#next()}</li>
+ * <li>Pass control to the next handler in the pipeline via {@link Exchange#next()}</li>
  * <li>Insert handlers into the pipeline via {@link Exchange#insert(java.util.List)} (or related methods) before passing on control</li>
  * <li>Forward the exchange to another handler (that it has a reference to) by directly calling its {@link #handle(Exchange)} method</li>
  * </ol>
  * <p>
  * A handler can either generate a response, decorate the response (e.g. add a response header) or direct the exchange to different handlers.
  * They are effectively a function that operates on the HTTP exchange.
- * <p>
  * <h3>Examples</h3>
- * Where there is no strict taxonomy of handlers, the following are indicative examples of common functions.
+ * While there is no strict taxonomy of handlers, the following are indicative examples of common functions.
  * <p>
- * <h4>Simple Responder</h4>
- * <p>
- * A simple "responder" type handler may just unconditionally return a response to the client.
- * <pre>
+ * <pre class="groovyTestCase">
+ * import org.ratpackframework.handling.*;
+ *
+ * // - SIMPLE RESPONDER
+ * // A simple "responder" type handler may just unconditionally return a response to the client.
+ *
  * class SimpleHandler implements Handler {
  *   void handle(Exchange exchange) {
  *     exchange.getResponse().send("Hello World!");
  *   }
  * }
- * </pre>
- * <h4>Conditional Responder</h4>
- * <p>
- * Or a handler may conditionally respond…
- * <pre>
+ *
+ *
+ * // - CONDITIONAL RESPONDER
+ * // Or a handler may conditionally respond…
+ *
  * class ConditionalHandler implements Handler {
  *   void handle(Exchange exchange) {
  *     if (exchange.getRequest().getPath().equals("foo")) {
@@ -72,11 +80,11 @@ import org.ratpackframework.api.NonBlocking;
  *     }
  *   }
  * }
- * </pre>
- * <h4>Router</h4>
- * <p>
- * A handler does not need to participate in the response, but can instead "route" the exchange to different handlers.
- * <pre>
+ *
+ *
+ * // - INSERTING ROUTER
+ * // A handler does not need to participate in the response, but can instead "route" the exchange to different handlers.
+ *
  * class RoutingHandler implements Handler {
  *   private final List&lt;Handler&gt; fooHandlers;
  *
@@ -92,12 +100,32 @@ import org.ratpackframework.api.NonBlocking;
  *     }
  *   }
  * }
+ *
+ * // - DELEGATING ROUTER
+ * // It can sometimes be appropriate to directly delegate to a handler, instead of using {@code insert()}.
+ *
+ * class FilteringHandler implements Handler {
+ *   private final Handler nestedHandler;
+ *
+ *   public FilteringHandler(Handler nestedHandler) {
+ *     this.nestedHandler = nestedHandler;
+ *   }
+ *
+ *   void handle(Exchange exchange) {
+ *     if (exchange.getRequest().getPath().startsWith("foo/")) {
+ *       nestedHandler.handle(exchange);
+ *     } else {
+ *       exchange.next();
+ *     }
+ *   }
+ * }
+ *
+ * Handler filtered = new FilteringHandler(new SimpleHandler());
  * </pre>
- * <p>
- * <i>This content is still work in progress.</i>
  *
  * @see Handlers
  * @see Chain
+ * @see org.ratpackframework.context.Context
  */
 public interface Handler {
 
