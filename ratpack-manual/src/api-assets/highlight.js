07df9537ae602af6c59a2f1ed36dbb33ce06f472
Luke Daley <ld@ldaley.com>
2013-05-30 12:08:03 +0100

    Only syntax highlight real code blocks.
    Prism eats links, so keep it away from the signatures.

--- ratpack-manual/src/api-assets/highlight.js
+++ ratpack-manual/src/api-assets/highlight.js
@@ -1,8 +1,10 @@
 var pres = document.getElementsByTagName("pre");
 for (var i = 0; i < pres.length; i++) {
   var pre = pres[i];
-  pre.className = pre.className + " language-groovy";
-  pre.innerHTML = "<code>" + pre.innerHTML.replace(/^\s+|\s+$/g, '') + "</code>";
-  Prism.highlightElement(pre, false, null);
+  if (pre.className.indexOf("groovyTestCase") != -1) {
+    pre.className = pre.className + " language-groovy";
+    pre.innerHTML = "<code>" + pre.innerHTML.replace(/^\s+|\s+$/g, '') + "</code>";
+    Prism.highlightElement(pre, false, null);
+  }
 }
 
