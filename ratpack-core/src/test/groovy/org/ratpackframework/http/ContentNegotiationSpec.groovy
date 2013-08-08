a1a3a800e58073617dda906befce25e6a7597ed0
Daniel Woods <daniel.woods@objectpartners.com>
2013-08-08 08:56:22 -0500

    rehoming the mime-type responder test

--- ratpack-core/src/test/groovy/org/ratpackframework/http/ContentNegotiationSpec.groovy
+++ ratpack-core/src/test/groovy/org/ratpackframework/http/ContentNegotiationSpec.groovy
@@ -16,6 +16,10 @@
 
 package org.ratpackframework.http
 
+import org.ratpackframework.groovy.handling.ClosureHandlers
+import org.ratpackframework.handling.Handlers
+
+import static org.ratpackframework.groovy.Util.with
 import static org.ratpackframework.groovy.Util.with
 import org.ratpackframework.test.groovy.RatpackGroovyDslSpec
 
@@ -80,5 +84,55 @@ class ContentNegotiationSpec extends RatpackGroovyDslSpec {
     then:
     getText("noneRegistered") == ""
     response.statusCode == 406
+  }
+
+  def "by accepts responder mime types"() {
+    setup:
+    "setting up the handler chain"
+    app {
+      handlers {
+        add Handlers.chain(
+          [ClosureHandlers.get {
+            with(accepts) {
+              json      { response.send "json" }
+              xml       { response.send "xml"  }
+              plainText { response.send "text" }
+              html      { response.send "html" }
+            }
+          }])
+      }
+    }
+
+    when:
+    "testing json"
+    request.header("Accept", "application/json")
+
+    then:
+    "the json accept type should be invoked"
+    text == "json"
+
+    when:
+    "testing xml"
+    request.header("Accept", "application/xml")
+
+    then:
+    "the xml accept type should be invoked"
+    text == "xml"
+
+    when:
+    "testing plainText"
+    request.header("Accept", "text/plain")
+
+    then:
+    "the plain text accept type should be invoked"
+    text == "text"
+
+    when:
+    "testing html"
+    request.header("Accept", "text/html")
+
+    then:
+    "the plain text accept type should be invoked"
+    text == "html"
   }
 }
