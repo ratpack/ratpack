ec83137350a7bc8ec3a48d94a79c1aacdf85b07b
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-08-16 23:25:19 +0100

    Re-skinned site & manual

---
+++ ratpack-manual/src/assets/js/manual.js
@@ -0,0 +1,20 @@
+WebFontConfig = {
+  google: { families: [ 'Engagement::latin', 'Merriweather:400,400italic,700:latin', 'Merriweather+Sans:700italic,700:latin' ] }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})();
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-41264608-1', 'ratpack-framework.org');
+ga('send', 'pageview');
