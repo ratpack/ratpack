afbc38118fe342d83cee2de095c12e760c7410bd
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-09-08 00:59:16 +0100

    #128 moved the keystore out to a resource file

--- ratpack-core/src/test/groovy/org/ratpackframework/http/HttpsSpec.groovy
+++ ratpack-core/src/test/groovy/org/ratpackframework/http/HttpsSpec.groovy
@@ -33,18 +33,13 @@ class HttpsSpec extends RatpackGroovyDslSpec {
   @Rule
   DummySSLContext clientContext = new DummySSLContext()
 
-  /**
-   * This is a keystore with a self-signed certificate.
-   */
-  private static
-  final String KEYSTORE = "/u3+7QAAAAIAAAABAAAAAQABMQAAATLccCWGAAABmTCCAZUwDgYKKwYBBAEqAhEBAQUABIIBgRRJBpHdCovt86mrLk8fwiBo02r5dt3cgbdN5XSuOzZIpHelji7YUbP3qYkGr87KCv+QLfWfxxoUAp0wuWJsHvBRNhHn9mQLE3URyzwlR5a+sBFrf0TFc1OrS0Kb4pFBjqfQ60MjGURVuhd6dvc1U2q88zsUgtlO0IhnzwjBWuOOugBV5JyTTsIiy04S3K6kO7TCRGliENniTgBZpBuRvJVTd3144o1c/x31QeanY5xb9Q4Cm2QqyzQ/mWH7klUyQD9S3/r+oouTfZOoWYPgvSIw1hhSj/OMV9ukqECHTb7yOFdOZ0j154inPTolJCSoEKo3/gkqHx1YCeUBCbchKRX6IMyT5kFLXUqZfRv+ZCu/GY+26B2XMDm2xUUNoJmlcs+X6yY8Nur+VFf/PqVJ8RXz7yAgLsQY3FsoztwJM0cLH7j6OZPerfuK2HVs1+m+luzWrHjU3SD+BdSkwbY45lKVTJ0J6iZSVpNHH5rR22/o9SCtFy5UMXLXUpou5j9twIsAAAABAAVYLjUwOQAAAmcwggJjMIICDaADAgECAgkAmA98MmCqaaAwDQYJKoZIhvcNAQEFBQAwVzELMAkGA1UEBhMCR0IxEzARBgNVBAgTClNvbWUtU3RhdGUxDzANBgNVBAcTBkxvbmRvbjEQMA4GA1UEChMHQmV0YW1heDEQMA4GA1UEAxMHQmV0YW1heDAeFw0xMTEwMDcwMzMwMjZaFw0xMTExMDYwMzMwMjZaMFcxCzAJBgNVBAYTAkdCMRMwEQYDVQQIEwpTb21lLVN0YXRlMQ8wDQYDVQQHEwZMb25kb24xEDAOBgNVBAoTB0JldGFtYXgxEDAOBgNVBAMTB0JldGFtYXgwXDANBgkqhkiG9w0BAQEFAANLADBIAkEAzI6qkBB90NRSsZreTazmrr/Rst58+SYyVuN2PgMkgDiQdz0aVChgZjhWnSMrg4ZyfY6fTUVL2DI0V6FV63WA2QIDAQABo4G7MIG4MB0GA1UdDgQWBBSdovSFYX8D+RvUCq6vJyGFFWAj7TCBiAYDVR0jBIGAMH6AFJ2i9IVhfwP5G9QKrq8nIYUVYCPtoVukWTBXMQswCQYDVQQGEwJHQjETMBEGA1UECBMKU29tZS1TdGF0ZTEPMA0GA1UEBxMGTG9uZG9uMRAwDgYDVQQKEwdCZXRhbWF4MRAwDgYDVQQDEwdCZXRhbWF4ggkAmA98MmCqaaAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAANBALmrm7MTl67KXaCQQmq4073m3X7hIsjsbWlPxh4MJ+/ankAqMO0OziaWdazJSj4L3uKAPLNcTvJ1KVrLqB6ArSDxBZYDmDSI0ZxUGN9eeo0FY/jWNw=="
   private static final String PASSWORD = "password"
 
   private static SSLContext createServerContext() {
     def sslContext = SSLContext.getInstance("TLS")
 
     def keyStore = KeyStore.getInstance("JKS")
-    keyStore.load(new ByteArrayInputStream(KEYSTORE.decodeBase64()), PASSWORD.toCharArray())
+    keyStore.load HttpsSpec.getResourceAsStream("dummy.keystore"), PASSWORD.toCharArray()
 
     def algorithm = Security.getProperty("ssl.KeyManagerFactory.algorithm") ?: "SunX509"
     def factory = KeyManagerFactory.getInstance(algorithm)
