7f2c7ea56ad723b76db98d35829a277b2603b7cb
Stefano Gualdi <stefano.gualdi@gmail.com>
2013-12-02 11:54:29 +0100

    Initial version for Thymeleaf templating engine module

---
+++ ratpack-thymeleaf/src/main/java/ratpack/thymeleaf/internal/ThymeleafTemplateRenderer.java
@@ -0,0 +1,32 @@
+package ratpack.thymeleaf.internal;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.IContext;
+import ratpack.file.MimeTypes;
+import ratpack.handling.Context;
+import ratpack.render.RendererSupport;
+import ratpack.thymeleaf.Template;
+
+import javax.inject.Inject;
+
+public class ThymeleafTemplateRenderer extends RendererSupport<Template<?>> {
+
+    private final TemplateEngine thymeleaf;
+
+    @Inject
+    public ThymeleafTemplateRenderer(TemplateEngine thymeleaf) {
+        this.thymeleaf = thymeleaf;
+    }
+
+    @Override
+    public void render(Context context, Template<?> template) {
+        String contentType = template.getContentType();
+        contentType = contentType == null ? context.get(MimeTypes.class).getContentType(template.getName()) : contentType;
+        try {
+            context.getResponse().send(contentType, thymeleaf.process(template.getName(), (IContext) template.getModel()));
+        }
+        catch (Exception e) {
+            context.error(e);
+        }
+    }
+}
