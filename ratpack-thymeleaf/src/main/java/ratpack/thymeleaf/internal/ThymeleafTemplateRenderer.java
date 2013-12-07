01b8264d88e831f8797557a177970878e987021a
Stefano Gualdi <stefano.gualdi@gmail.com>
2013-12-07 15:53:07 +0100

    Fix checkstyle's violations

--- ratpack-thymeleaf/src/main/java/ratpack/thymeleaf/internal/ThymeleafTemplateRenderer.java
+++ ratpack-thymeleaf/src/main/java/ratpack/thymeleaf/internal/ThymeleafTemplateRenderer.java
@@ -1,7 +1,22 @@
+/*
+ * Copyright 2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.thymeleaf.internal;
 
 import org.thymeleaf.TemplateEngine;
-import ratpack.file.MimeTypes;
 import ratpack.handling.Context;
 import ratpack.render.RendererSupport;
 import ratpack.thymeleaf.Template;
@@ -10,22 +25,21 @@ import javax.inject.Inject;
 
 public class ThymeleafTemplateRenderer extends RendererSupport<Template> {
 
-    private final TemplateEngine thymeleaf;
+  private final TemplateEngine thymeleaf;
 
-    @Inject
-    public ThymeleafTemplateRenderer(TemplateEngine thymeleaf) {
-        this.thymeleaf = thymeleaf;
-    }
+  @Inject
+  public ThymeleafTemplateRenderer(TemplateEngine thymeleaf) {
+    this.thymeleaf = thymeleaf;
+  }
 
-    @Override
-    public void render(Context context, Template template) {
-        String contentType = template.getContentType();
-        contentType = contentType == null ? "text/html" : contentType;
-        try {
-            context.getResponse().send(contentType, thymeleaf.process(template.getName(), template.getModel()));
-        }
-        catch (Exception e) {
-            context.error(e);
-        }
+  @Override
+  public void render(Context context, Template template) {
+    String contentType = template.getContentType();
+    contentType = contentType == null ? "text/html" : contentType;
+    try {
+      context.getResponse().send(contentType, thymeleaf.process(template.getName(), template.getModel()));
+    } catch (Exception e) {
+      context.error(e);
     }
+  }
 }
