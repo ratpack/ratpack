c243bd87187ce84155707edeb3a780b84cc2d421
Luke Daley <ld@ldaley.com>
2013-12-24 15:01:31 +0000

    Inline the ModuleRegistry impl so we don't have to widen its API for the internal protocol.

--- ratpack-guice/src/main/java/ratpack/guice/internal/DefaultGuiceBackedHandlerFactory.java
+++ ratpack-guice/src/main/java/ratpack/guice/internal/DefaultGuiceBackedHandlerFactory.java
@@ -16,6 +16,8 @@
 
 package ratpack.guice.internal;
 
+import com.google.common.collect.ImmutableList;
+import com.google.inject.Binder;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.util.Modules;
@@ -27,16 +29,16 @@ import ratpack.handling.Handlers;
 import ratpack.handling.internal.ClientErrorForwardingHandler;
 import ratpack.handling.internal.FactoryHandler;
 import ratpack.launch.LaunchConfig;
+import ratpack.registry.NotInRegistryException;
 import ratpack.reload.internal.ClassUtil;
 import ratpack.reload.internal.ReloadableFileBackedFactory;
 import ratpack.util.Action;
 import ratpack.util.Factory;
 import ratpack.util.Transformer;
 
+import javax.inject.Provider;
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 import static ratpack.util.ExceptionUtils.uncheck;
 
@@ -88,7 +90,7 @@ public class DefaultGuiceBackedHandlerFactory implements GuiceBackedHandlerFacto
 
     Injector injector = moduleTransformer.transform(masterModule);
 
-    List<Action<Injector>> init = moduleRegistry.getInit();
+    List<Action<Injector>> init = moduleRegistry.init;
     for (Action<Injector> initAction : init) {
       initAction.execute(injector);
     }
@@ -117,4 +119,151 @@ public class DefaultGuiceBackedHandlerFactory implements GuiceBackedHandlerFacto
     moduleRegistry.register(new DefaultRatpackModule(launchConfig));
   }
 
+  private static class DefaultModuleRegistry implements ModuleRegistry {
+
+    private final Map<Class<? extends Module>, Module> modules = new LinkedHashMap<>();
+    private final LaunchConfig launchConfig;
+
+    private final LinkedList<Action<Binder>> actions = new LinkedList<>();
+    private final LinkedList<Action<Injector>> init = new LinkedList<>();
+
+    public DefaultModuleRegistry(LaunchConfig launchConfig) {
+      this.launchConfig = launchConfig;
+    }
+
+    public LaunchConfig getLaunchConfig() {
+      return launchConfig;
+    }
+
+    public void bind(final Class<?> type) {
+      actions.add(new Action<Binder>() {
+        public void execute(Binder binder) {
+          binder.bind(type);
+        }
+      });
+    }
+
+    public <T> void bind(final Class<T> publicType, final Class<? extends T> implType) {
+      actions.add(new Action<Binder>() {
+        public void execute(Binder binder) {
+          binder.bind(publicType).to(implType);
+        }
+      });
+    }
+
+    public <T> void bind(final Class<? super T> publicType, final T instance) {
+      actions.add(new Action<Binder>() {
+        public void execute(Binder binder) {
+          binder.bind(publicType).toInstance(instance);
+        }
+      });
+    }
+
+    public <T> void bind(final T instance) {
+      @SuppressWarnings("unchecked") final
+      Class<T> type = (Class<T>) instance.getClass();
+      actions.add(new Action<Binder>() {
+        public void execute(Binder binder) {
+          binder.bind(type).toInstance(instance);
+        }
+      });
+    }
+
+    public <T> void provider(final Class<T> publicType, final Class<? extends Provider<? extends T>> providerType) {
+      actions.add(new Action<Binder>() {
+        public void execute(Binder binder) {
+          binder.bind(publicType).toProvider(providerType);
+        }
+      });
+    }
+
+    @SuppressWarnings("unchecked")
+    public void register(Module module) {
+      register((Class<Module>) module.getClass(), module);
+    }
+
+    public <O extends Module> void register(Class<O> type, O module) {
+      if (modules.containsKey(type)) {
+        Object existing = modules.get(type);
+        throw new IllegalArgumentException(String.format("Module '%s' is already registered with type '%s' (attempting to register '%s')", existing, type, module));
+      }
+
+      modules.put(type, module);
+    }
+
+    public <T> T get(Class<T> moduleType) {
+      @SuppressWarnings("SuspiciousMethodCalls") Module module = modules.get(moduleType);
+      if (module == null) {
+        throw new NotInRegistryException(moduleType);
+      }
+
+      return moduleType.cast(module);
+    }
+
+    @Override
+    public <O> List<O> getAll(Class<O> type) {
+      ImmutableList.Builder<O> builder = ImmutableList.builder();
+      for (Module module : modules.values()) {
+        if (type.isInstance(module)) {
+          @SuppressWarnings("unchecked") O cast = (O) module;
+          builder.add(cast);
+        }
+      }
+      return builder.build();
+    }
+
+    public <O> O maybeGet(Class<O> type) {
+      @SuppressWarnings("SuspiciousMethodCalls") Module module = modules.get(type);
+      if (module == null) {
+        return null;
+      }
+
+      return type.cast(module);
+    }
+
+    public <T extends Module> T remove(Class<T> moduleType) {
+      if (!modules.containsKey(moduleType)) {
+        throw new NotInRegistryException(moduleType);
+      }
+
+      return moduleType.cast(modules.remove(moduleType));
+    }
+
+    private List<Module> getModules() {
+      return ImmutableList.<Module>builder()
+        .addAll(modules.values())
+        .add(createOverrideModule())
+        .build();
+    }
+
+    private Module createOverrideModule() {
+      return new Module() {
+        public void configure(Binder binder) {
+          for (Action<Binder> binderAction : actions) {
+            try {
+              binderAction.execute(binder);
+            } catch (Exception e) {
+              throw uncheck(e);
+            }
+          }
+        }
+      };
+    }
+
+    @Override
+    public void init(Action<Injector> action) {
+      init.add(action);
+    }
+
+    @Override
+    public void init(final Class<? extends Runnable> clazz) {
+      init.add(new Action<Injector>() {
+        @Override
+        public void execute(Injector injector) throws Exception {
+          injector.getInstance(clazz).run();
+        }
+      });
+    }
+
+  }
 }
