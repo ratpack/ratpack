db6208edfe3ae4cec954d600f25856b67870d246
Luke Daley <ld@ldaley.com>
2014-03-18 11:53:40 +1000

    Fix case issue.

--- ratpack-manual/src/test/groovy/ratpack/manual/snippets/fixtures/GradleFixture.groovy
+++ ratpack-manual/src/test/groovy/ratpack/manual/snippets/fixtures/GradleFixture.groovy
@@ -37,7 +37,7 @@ def script = '''
 
   @Override
   String post() {
-    String localRepo = System.getProperty("localRepo", "build/localRepo")
+    String localRepo = System.getProperty("localRepo", "build/localrepo")
     def localRepoPath = new File(localRepo).canonicalPath
     """
 '''
