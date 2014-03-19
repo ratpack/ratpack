abc442b86ceab09a16e36bf9b604fbd9d58e1084
David M. Carr <david@carrclan.us>
2014-03-18 23:36:01 -0400

    Remove ratpack-heroku Gradle plugin (closes #269)

--- ratpack-gradle/src/main/resources/heroku/.profile.d/setenv.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-echo "Setting environment variables..."
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xmx512m -XX:+UseCompressedOops -Dratpack.port=\${PORT}"
-export JAVA_HOME="\${HOME}/.jdk7"
