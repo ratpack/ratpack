05b163c8e5e312ce8e42951a53e34936fb6810f4
rhart <russellxhart@googlemail.com>
2014-03-28 15:10:38 +0000

    Statically compile Groovy template for performance test app

--- perf/src/apps/template-engine-groovy/src/ratpack/ratpack.groovy
+++ perf/src/apps/template-engine-groovy/src/ratpack/ratpack.groovy
@@ -1,13 +1,16 @@
+import ratpack.groovy.templating.TemplatingModule
 import ratpack.handlebars.HandlebarsModule
 import ratpack.perf.incl.*
 import ratpack.thymeleaf.ThymeleafModule
 
-import static ratpack.groovy.Groovy.*
+import static ratpack.groovy.Groovy.groovyTemplate
+import static ratpack.groovy.Groovy.ratpack
 import static ratpack.handlebars.Template.handlebarsTemplate
 import static ratpack.thymeleaf.Template.thymeleafTemplate
 
 ratpack {
   modules {
+    get(TemplatingModule).staticallyCompile = true
     register new HandlebarsModule()
     register new ThymeleafModule()
   }
