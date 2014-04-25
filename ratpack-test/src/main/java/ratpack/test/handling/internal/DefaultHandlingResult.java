6783b54acb7ca79b98bd013f242d6845560b94e7
Luke Daley <ld@ldaley.com>
2014-04-25 15:46:02 +1000

    Shutdown the event loop after the request when unit testing.

--- ratpack-test/src/main/java/ratpack/test/handling/internal/DefaultHandlingResult.java
+++ ratpack-test/src/main/java/ratpack/test/handling/internal/DefaultHandlingResult.java
@@ -180,6 +180,12 @@ public class DefaultHandlingResult implements HandlingResult {
       }
     } catch (InterruptedException e) {
       throw uncheck(e); // what to do here?
+    } finally {
+      try {
+        launchConfig.getExecController().shutdown();
+      } catch (Exception ignore) {
+        // swallow
+      }
     }
   }
 
