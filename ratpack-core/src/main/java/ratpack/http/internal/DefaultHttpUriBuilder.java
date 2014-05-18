bf75a912139ed87191816be63733ca8ab045f5c1
Roy Schumacher <royjschumacher@gmail.com>
2014-05-18 19:54:26 +0100

    added HttpUriBuilder interface to main.java.ratpack.http and DefaultHttpUriBuilder class to main.java.ratpack.http.internal. included new test class in the http internal tests package.

---
+++ ratpack-core/src/main/java/ratpack/http/internal/DefaultHttpUriBuilder.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ratpack.http.internal;
+
+import com.google.common.collect.Multimap;
+import com.google.common.escape.Escaper;
+import com.google.common.net.UrlEscapers;
+import com.sun.jndi.toolkit.url.Uri;
+import ratpack.func.Action;
+import ratpack.http.HttpUriBuilder;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class DefaultHttpUriBuilder implements HttpUriBuilder {
+
+  private String host;
+  private String path;
+  private String pathComponent;
+  private String protocol;
+  private String port;
+
+  private Action<? super  Multimap<String, String>> params;
+
+  private Escaper escaper;
+
+
+  public DefaultHttpUriBuilder(){
+    this.protocol = "http://";
+    this.path = "";
+    this.pathComponent = "";
+    this.port = "";
+  }
+
+
+  @Override
+  public HttpUriBuilder secure() {
+    this.protocol = "https://";
+    return this;
+  }
+
+  @Override
+  public HttpUriBuilder host(String host) {
+    escaper = UrlEscapers.urlFragmentEscaper();
+    this.host = escaper.escape(host);
+    return this;
+  }
+
+  @Override
+  public HttpUriBuilder port(int port) {
+    this.port = String.format("%s%d%s",":",port,"/");
+    return this;
+  }
+
+  @Override
+  public HttpUriBuilder path(String path) {
+    escaper = UrlEscapers.urlFragmentEscaper();
+    this.path = escaper.escape(path).concat("/");
+    return this;
+  }
+
+  @Override
+  public HttpUriBuilder pathComponent(String pathComponent) {
+    escaper = UrlEscapers.urlPathSegmentEscaper();
+    this.pathComponent = escaper.escape(pathComponent);
+    return this;
+  }
+
+  @Override
+  public HttpUriBuilder params(Action<? super Multimap<String, String>> params) {
+    this.params = params;
+    return this;
+  }
+
+  @Override
+  public URI build() throws URISyntaxException {
+    URI uri = new URI(this.toString());
+    return uri;
+  }
+
+  @Override
+  public String toString() {
+    return protocol + host + port + path + pathComponent;
+  }
+}
---
+++ ratpack-core/src/test/groovy/ratpack/http/internal/BuildsEncodedUri.groovy
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ratpack.http.internal
+
+import ratpack.http.HttpUriBuilder
+import ratpack.test.internal.RatpackGroovyDslSpec
+
+/**
+ * Created by roy on 18/05/14.
+ */
+class BuildsEncodedUri extends RatpackGroovyDslSpec {
+
+  def "builds and encodes uri with all parameters correctly"() {
+    String value;
+    when:
+      HttpUriBuilder builder = new DefaultHttpUriBuilder()
+      value = builder.secure().host("foo.com").port(1234).path("foo/bar").pathComponent("foo/bar").build().toString();
+
+    then:
+      value ==  "https://foo.com:1234/foo/bar/foo%2Fbar";
+  }
+
+  def "builds and encodes uri when missing parmeters"(){
+    String value;
+    when:
+    value = new DefaultHttpUriBuilder().secure().host("foo.com").path("foo").pathComponent("foo/bar").build().toString();
+
+    then:
+    value ==  "https://foo.com/foo/foo%2Fbar";
+  }
+}
