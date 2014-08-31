425afd9d0cdf7e0117e9e5d427a2537089ba4d98
Luke Daley <ld@ldaley.com>
2014-08-31 11:38:39 +1000

    Expand server side event rendering example.

--- ratpack-core/src/main/java/ratpack/sse/ServerSentEvents.java
+++ ratpack-core/src/main/java/ratpack/sse/ServerSentEvents.java
@@ -19,29 +19,86 @@ package ratpack.sse;
 import org.reactivestreams.Publisher;
 
 /**
- * A renderer for streaming Server Sent Events.
+ * A {@link ratpack.handling.Context#render(Object) renderable} object for streaming server side events.
  * <p>
- * An implementation of this is <b>always</b> provided by Ratpack core.
+ * A {@link ratpack.render.Renderer renderer} for this type is implicitly provided by Ratpack core.
  * <p>
  * Example usage:
- * <pre class="tested">
+ * <pre class="java">
  * import ratpack.handling.Handler;
  * import ratpack.handling.Context;
+ * import ratpack.func.Function;
+ * import ratpack.stream.Streams;
+ * import ratpack.launch.HandlerFactory;
+ * import ratpack.launch.LaunchConfig;
+ * import ratpack.launch.LaunchConfigBuilder;
+ * import ratpack.http.client.ReceivedResponse;
  * import ratpack.sse.ServerSentEvent;
+ * import ratpack.test.embed.EmbeddedApplication;
+ * import ratpack.test.embed.LaunchConfigEmbeddedApplication;
+ * import ratpack.test.http.TestHttpClient;
+ * import ratpack.test.http.TestHttpClients;
+ *
+ * import static ratpack.sse.ServerSentEvents.serverSentEvents;
+ *
+ * import java.util.List;
+ * import java.util.concurrent.TimeUnit;
+ * import java.util.concurrent.ScheduledExecutorService;
  * import org.reactivestreams.Publisher;
- * import static ratpack.sse.ServerSentEvents.serverSentEvents
  *
- * public class ServerSentEventRenderingHandler implements Handler {
+ * import com.google.common.collect.Lists;
+ * import com.google.common.base.Joiner;
  *
- *   private Publisher&lt;ServerSentEvent&gt; chunkStream;
+ * public class Example {
  *
- *   public ServerSentEventRenderingHandler(Publisher&lt;ServerSentEvent&gt; chunkStream) {
- *     this.chunkStream = chunkStream;
+ *   private static EmbeddedApplication createApp() {
+ *     return new LaunchConfigEmbeddedApplication() {
+ *       protected LaunchConfig createLaunchConfig() {
+ *         return LaunchConfigBuilder.noBaseDir().port(0).build(new HandlerFactory() {
+ *             public Handler create(LaunchConfig launchConfig) {
+ *
+ *               // Example of streaming chunks
+ *
+ *               return new Handler() {
+ *                 public void handle(Context context) {
+ *                   // simulate streaming by periodically publishing
+ *                   ScheduledExecutorService executor = context.getLaunchConfig().getExecController().getExecutor();
+ *                   Publisher&lt;ServerSentEvent&gt; eventStream = Streams.periodically(executor, 5, TimeUnit.MILLISECONDS, new Function&lt;Integer, ServerSentEvent&gt;() {
+ *                     public ServerSentEvent apply(Integer i) {
+ *                       if (i.intValue() &lt; 5) {
+ *                         return new ServerSentEvent(i.toString(), "counter", "event " + i);
+ *                       } else {
+ *                         return null;
+ *                       }
+ *                     }
+ *                   });
+ *
+ *                   context.render(serverSentEvents(eventStream));
+ *                 }
+ *               };
+ *
+ *             }
+ *           });
+ *       }
+ *     };
  *   }
  *
- *   public void handle(Context context) {
- *     context.render(serverSentEvents(chunkStream));
+ *   public static void main(String[] args) {
+ *     try(EmbeddedApplication app = createApp()) {
+ *       ReceivedResponse response = app.getHttpClient().get();
+ *       assert response.getHeaders().get("Content-Type").equals("text/event-stream;charset=UTF-8");
+ *
+ *       List&lt;String&gt; outputEvents = Lists.transform(Lists.newArrayList(0, 1, 2, 3, 4), new com.google.common.base.Function&lt;Integer, String&gt;() {
+ *         public String apply(Integer i) {
+ *           return "event: counter\ndata: event " + i + "\nid: " + i + "\n";
+ *         }
+ *       });
+ *
+ *       String expectedOutput = Joiner.on("\n").join(outputEvents) + "\n";
+ *       assert response.getBody().getText().equals(expectedOutput);
+ *     }
  *   }
+ *
  * }
  * </pre>
  *
