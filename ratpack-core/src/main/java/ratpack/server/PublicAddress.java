269d713ac4d0b46c46535be255e9a9c2cf437784
David M. Carr <david@carrclan.us>
2014-09-02 22:04:48 -0400

    Update PublicAddress JavaDoc to discuss default behavior #431

--- ratpack-core/src/main/java/ratpack/server/PublicAddress.java
+++ ratpack-core/src/main/java/ratpack/server/PublicAddress.java
@@ -21,7 +21,23 @@ import ratpack.handling.Context;
 import java.net.URI;
 
 /**
- * The advertised public address.
+ * The advertised public address.  This is most commonly used to perform redirects or assemble absolute URLs.
+ * <p>
+ * The default implementation uses a variety of strategies to attempt to provide the desired result most of the time.
+ * Information used includes:
+ * <ul>
+ *   <li>Configured public address URI (optional)</li>
+ *   <li>X-Forwarded-Host header (if included in request)</li>
+ *   <li>X-Forwarded-Proto or X-Forwarded-Ssl headers (if included in request)</li>
+ *   <li>Absolute request URI (if included in request)</li>
+ *   <li>Host header (if included in request)</li>
+ *   <li>Service's bind address and scheme (http vs. https)</li>
+ * </ul>
+ *
+ * <p>
+ * If the default implementation isn't doing what you want it to, you can usually simply configure the public address via
+ * {@link ratpack.launch.LaunchConfigBuilder#publicAddress(java.net.URI)}.  Alternatively, you can register an instance of
+ * your own implementation.
  */
 public interface PublicAddress {
 
