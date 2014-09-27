7da7cc5534a369c9a61e89b5b5aa1c1fdc38c48b
Luke Daley <ld@ldaley.com>
2014-09-27 13:52:05 +1000

    Add some sugar for embedded applications.

--- ratpack-test/src/main/java/ratpack/test/embed/EmbeddedApplication.java
+++ ratpack-test/src/main/java/ratpack/test/embed/EmbeddedApplication.java
@@ -16,9 +16,20 @@
 
 package ratpack.test.embed;
 
+import ratpack.func.Action;
+import ratpack.func.Function;
+import ratpack.handling.Chain;
+import ratpack.handling.Handler;
+import ratpack.handling.Handlers;
+import ratpack.launch.LaunchConfig;
+import ratpack.launch.LaunchConfigBuilder;
 import ratpack.server.RatpackServer;
 import ratpack.test.ApplicationUnderTest;
 import ratpack.test.http.TestHttpClient;
+
+import java.util.function.Consumer;
+
+import static ratpack.util.ExceptionUtils.uncheck;
 
 /**
  * An application created and used at runtime, useful for functionally testing subsets of functionality.
@@ -35,6 +46,75 @@ import ratpack.test.http.TestHttpClient;
  * @see ratpack.test.embed.LaunchConfigEmbeddedApplication
  */
 public interface EmbeddedApplication extends ApplicationUnderTest, AutoCloseable {
+
+  /**
+   * Creates an embedded application by building a {@link LaunchConfig}.
+   * <p>
+   * The given {@link LaunchConfigBuilder} will be configured to not have base dir, and to use an ephemeral port.
+   *
+   * @param function a function that builds a launch config from a launch config builder
+   * @return a newly created embedded application
+   */
+  static EmbeddedApplication fromLaunchConfigBuilder(Function<? super LaunchConfigBuilder, ? extends LaunchConfig> function) {
+    return new LaunchConfigEmbeddedApplication() {
+      @Override
+      protected LaunchConfig createLaunchConfig() {
+        return uncheck(() -> function.apply(LaunchConfigBuilder.noBaseDir().port(0)));
+      }
+    };
+  }
+
+  /**
+   * Creates an embedded application with a default launch config (no base dir, ephemeral port) and the given handler.
+   * <p>
+   * If you need to tweak the launch config, use {@link #fromLaunchConfigBuilder(Function)}.
+   *
+   * @param handler the application handler
+   * @return a newly created embedded application
+   */
+  static EmbeddedApplication fromHandler(Handler handler) {
+    return fromLaunchConfigBuilder(lcb -> lcb.build(lc -> handler));
+  }
+
+  /**
+   * Creates an embedded application with a default launch config (no base dir, ephemeral port) and the given handler chain.
+   * <p>
+   * If you need to tweak the launch config, use {@link #fromLaunchConfigBuilder(Function)}.
+   *
+   * @param action the handler chain definition
+   * @return a newly created embedded application
+   */
+  static EmbeddedApplication fromChain(Action<? super Chain> action) {
+    return fromLaunchConfigBuilder(lcb -> lcb.build(lc -> Handlers.chain(lc, action)));
+  }
+
+  /**
+   * Provides the given consumer with a {@link #getHttpClient() test http client} for this application, then closes this application.
+   * <p>
+   * The application will be closed regardless of whether the given consumer throws an exception.
+   * <pre class="java">
+   *
+   * import ratpack.test.embed.EmbeddedApplication;
+   *
+   * public class Example {
+   *   public static void main(String... args) {
+   *     EmbeddedApplication.fromHandler(ctx -> ctx.render("ok"))
+   *       .test(httpClient -> {
+   *         assert httpClient.get().getBody().getText().equals("ok");
+   *       });
+   *   }
+   * }
+   * </pre>
+   *
+   * @param consumer a consumer that tests this embedded application
+   */
+  default void test(Consumer<? super TestHttpClient> consumer) {
+    try {
+      consumer.accept(getHttpClient());
+    } finally {
+      close();
+    }
+  }
 
   /**
    * The server for the application.
