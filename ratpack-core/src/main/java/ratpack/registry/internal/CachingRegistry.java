9a130afe536b9eb408fe0073c4f5afe8a75753d7
Luke Daley <ld@ldaley.com>
2015-01-04 10:52:16 +1000

    Use ConcurrentMap instead of a LoadingCache for registry caching.

--- ratpack-core/src/main/java/ratpack/registry/internal/CachingRegistry.java
+++ ratpack-core/src/main/java/ratpack/registry/internal/CachingRegistry.java
@@ -17,49 +17,24 @@
 package ratpack.registry.internal;
 
 import com.google.common.base.Predicate;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import com.google.common.collect.Lists;
+import com.google.common.collect.Iterables;
 import com.google.common.reflect.TypeToken;
-import com.google.common.util.concurrent.UncheckedExecutionException;
 import ratpack.func.Action;
 import ratpack.registry.PredicateCacheability;
 import ratpack.registry.Registry;
+import ratpack.util.Types;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.ExecutionException;
-
-import static ratpack.util.ExceptionUtils.toException;
-import static ratpack.util.ExceptionUtils.uncheck;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class CachingRegistry implements Registry {
 
   private final Registry delegate;
 
-  private final LoadingCache<TypeToken<?>, ? extends Optional<?>> cache = CacheBuilder.newBuilder().build(new CacheLoader<TypeToken<?>, Optional<?>>() {
-    public Optional<?> load(@SuppressWarnings("NullableProblems") TypeToken<?> key) throws Exception {
-      return delegate.maybeGet(key);
-    }
-  });
-
-  private final LoadingCache<TypeToken<?>, List<?>> allCache = CacheBuilder.newBuilder().build(new CacheLoader<TypeToken<?>, List<?>>() {
-    public List<?> load(@SuppressWarnings("NullableProblems") TypeToken<?> key) throws Exception {
-      return Lists.newArrayList(delegate.getAll(key));
-    }
-  });
-
-  private LoadingCache<PredicateCacheability.CacheKey<?>, List<?>> predicateCache = CacheBuilder.newBuilder().build(new CacheLoader<PredicateCacheability.CacheKey<?>, List<?>>() {
-    @Override
-    public List<?> load(@SuppressWarnings("NullableProblems") PredicateCacheability.CacheKey<?> key) throws Exception {
-      return Lists.newArrayList(getAll(key));
-    }
-
-    private <T> Iterable<? extends T> getAll(PredicateCacheability.CacheKey<T> key) {
-      return delegate.all(key.type, key.predicate);
-    }
-  });
+  private final ConcurrentMap<TypeToken<?>, Optional<?>> cache = new ConcurrentHashMap<>();
+  private final ConcurrentMap<TypeToken<?>, Iterable<?>> allCache = new ConcurrentHashMap<>();
+  private ConcurrentMap<PredicateCacheability.CacheKey<?>, Iterable<?>> predicateCache = new ConcurrentHashMap<>();
 
   public CachingRegistry(Registry delegate) {
     this.delegate = delegate;
@@ -67,42 +42,23 @@ public class CachingRegistry implements Registry {
 
   @Override
   public <O> Optional<O> maybeGet(TypeToken<O> type) {
-    try {
-      @SuppressWarnings("unchecked") Optional<O> o = (Optional<O>) cache.get(type);
-      return o;
-    } catch (ExecutionException | UncheckedExecutionException e) {
-      throw uncheck(toException(e.getCause()));
-    }
+    return Types.cast(cache.computeIfAbsent(type, delegate::maybeGet));
   }
 
   @Override
-  public <O> List<O> getAll(TypeToken<O> type) {
-    try {
-      @SuppressWarnings("unchecked") List<O> objects = (List<O>) allCache.get(type);
-      return objects;
-    } catch (ExecutionException | UncheckedExecutionException e) {
-      throw uncheck(toException(e.getCause()));
-    }
+  public <O> Iterable<O> getAll(TypeToken<O> type) {
+    return Types.cast(allCache.computeIfAbsent(type, delegate::getAll));
   }
 
-  private <T> List<T> getFromPredicateCache(TypeToken<T> type, Predicate<? super T> predicate) {
-    try {
-      @SuppressWarnings("unchecked") List<T> objects = (List<T>) predicateCache.get(new PredicateCacheability.CacheKey<>(type, predicate));
-      return objects;
-    } catch (ExecutionException | UncheckedExecutionException e) {
-      throw uncheck(toException(e.getCause()));
-    }
+  private <T> Iterable<? extends T> getFromPredicateCache(TypeToken<T> type, Predicate<? super T> predicate) {
+    return Types.cast(predicateCache.computeIfAbsent(new PredicateCacheability.CacheKey<>(type, predicate), k -> delegate.all(type, predicate)));
   }
 
   @Override
   public <T> Optional<T> first(TypeToken<T> type, Predicate<? super T> predicate) {
     if (PredicateCacheability.isCacheable(predicate)) {
-      List<T> objects = getFromPredicateCache(type, predicate);
-      if (objects.isEmpty()) {
-        return Optional.empty();
-      } else {
-        return Optional.of(objects.get(0));
-      }
+      Iterable<? extends T> objects = getFromPredicateCache(type, predicate);
+      return Optional.ofNullable(Iterables.getFirst(objects, null));
     } else {
       return delegate.first(type, predicate);
     }
