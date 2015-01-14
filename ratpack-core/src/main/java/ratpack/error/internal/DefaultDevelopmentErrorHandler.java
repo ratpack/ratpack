6871f6b9b0101912de310a421a78f6d868f9d939
David M. Carr <david@carrclan.us>
2015-01-13 22:39:05 -0500

    Add noMatch handling to default development error handler (#548).

--- ratpack-core/src/main/java/ratpack/error/internal/DefaultDevelopmentErrorHandler.java
+++ ratpack-core/src/main/java/ratpack/error/internal/DefaultDevelopmentErrorHandler.java
@@ -64,6 +64,7 @@ public class DefaultDevelopmentErrorHandler implements ErrorHandler {
               }
             }
         )
+        .noMatch("text/plain")
     );
 
   }
@@ -101,6 +102,7 @@ public class DefaultDevelopmentErrorHandler implements ErrorHandler {
               }
             }
         )
+        .noMatch("text/plain")
     );
   }
 
--- ratpack-core/src/test/groovy/ratpack/error/DevelopmentErrorHandlerSpec.groovy
+++ ratpack-core/src/test/groovy/ratpack/error/DevelopmentErrorHandlerSpec.groovy
@@ -70,4 +70,30 @@ class DevelopmentErrorHandlerSpec extends RatpackGroovyDslSpec {
       body.contentType.text
     }
   }
+
+  def "debug error handler prints text if content type not matched"() {
+    given:
+    def e = new RuntimeException("!")
+    requestSpec { it.headers.add("Accept", "application/json") }
+
+    when:
+    serverConfig { development(true) }
+    handlers {
+      get("client") { clientError(404) }
+      get("server") { error(e) }
+    }
+
+    then:
+    with(get("client")) {
+      statusCode == 404
+      body.text.startsWith("Client error 404")
+      body.contentType.text
+    }
+
+    with(get("server")) {
+      statusCode == 500
+      body.text.startsWith(RuntimeException.name)
+      body.contentType.text
+    }
+  }
 }
