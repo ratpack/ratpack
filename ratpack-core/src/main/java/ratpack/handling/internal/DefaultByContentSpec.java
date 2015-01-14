10e463cbf77df5aea44700d4062d5c135245dba7
David M. Carr <david@carrclan.us>
2015-01-13 19:11:04 -0500

    Revert "Add `allTypes` to ByContentSpec."
    This reverts commit 40bb4d6639d4c920adda81b45c169697a9f62d7b.

--- ratpack-core/src/main/java/ratpack/handling/internal/DefaultByContentSpec.java
+++ ratpack-core/src/main/java/ratpack/handling/internal/DefaultByContentSpec.java
@@ -18,7 +18,6 @@ package ratpack.handling.internal;
 
 import ratpack.func.NoArgAction;
 import ratpack.handling.ByContentSpec;
-import ratpack.http.internal.MimeParse;
 
 import java.util.Map;
 
@@ -58,11 +57,6 @@ public class DefaultByContentSpec implements ByContentSpec {
 
   public ByContentSpec xml(NoArgAction handler) {
     return type("application/xml", handler);
-  }
-
-  @Override
-  public ByContentSpec allTypes(NoArgAction handler) {
-    return type(MimeParse.MIME_ANY, handler);
   }
 
 }
