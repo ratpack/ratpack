4847f98c81fe17427606b440ac6082d42dcb7f64
rhart <russellxhart@googlemail.com>
2015-01-29 18:18:11 +0000

    Fix race condition in `ContentStreamingRequestActionSpec`

--- ratpack-core/src/test/groovy/ratpack/http/client/internal/ContentStreamingRequestActionSpec.groovy
+++ ratpack-core/src/test/groovy/ratpack/http/client/internal/ContentStreamingRequestActionSpec.groovy
@@ -18,9 +18,7 @@ package ratpack.http.client.internal
 
 import io.netty.buffer.ByteBufAllocator
 import io.netty.channel.Channel
-import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelPipeline
-import io.netty.channel.SimpleChannelInboundHandler
 import ratpack.exec.ExecController
 import ratpack.exec.Execution
 import ratpack.exec.Fulfiller
@@ -29,22 +27,25 @@ import ratpack.http.client.HttpClientSpec
 import ratpack.http.client.RequestSpec
 import ratpack.http.client.StreamedResponse
 
+import java.util.concurrent.CountDownLatch
+
 class ContentStreamingRequestActionSpec extends HttpClientSpec {
 
   def "client channel is closed when response is not subscribed to"() {
     def requestAction
+    def latch = new CountDownLatch(1)
 
     given:
     otherApp { get("foo") { render "bar" } }
 
     and:
     handlers {
-      get {
-        ExecController execController = get(ExecController)
-        ByteBufAllocator byteBufAllocator = get(ByteBufAllocator)
-
+      get { ExecController execController, ByteBufAllocator byteBufAllocator ->
         requestAction = new ChannelSpyRequestAction({}, otherAppUrl("foo"), execution, byteBufAllocator)
         execController.control.promise(requestAction).then {
+          execution.onCleanup {
+            latch.countDown()
+          }
           render 'foo'
         }
       }
@@ -52,6 +53,7 @@ class ContentStreamingRequestActionSpec extends HttpClientSpec {
 
     expect:
     text == 'foo'
+    latch.await()
     !requestAction.channel.open
   }
 
@@ -64,14 +66,8 @@ class ContentStreamingRequestActionSpec extends HttpClientSpec {
 
     @Override
     protected void addResponseHandlers(ChannelPipeline p, Fulfiller<? super StreamedResponse> fulfiller) {
+      channel = p.channel()
       super.addResponseHandlers(p, fulfiller)
-      p.addFirst(new SimpleChannelInboundHandler(false) {
-        @Override
-        protected void channelRead0(ChannelHandlerContext ctx, Object msg) throws Exception {
-          channel = ctx.channel()
-          ctx.fireChannelRead(msg)
-        }
-      })
     }
   }
 }
