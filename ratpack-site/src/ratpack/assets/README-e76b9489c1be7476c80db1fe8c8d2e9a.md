ae82295febf5923bc51b3fd18a97ec28c8978fd5
David Estes <davydotcom@gmail.com>
2015-06-14 08:13:34 -0400

    clearing out unnecessary repo dependencies and trueing up gradle file

--- ratpack-site/src/ratpack/assets/README-e76b9489c1be7476c80db1fe8c8d2e9a.md
+++
@@ -1,1 +0,0 @@
-This directory contains Sass files used by both *ratpack-site* and *ratpack-manual*.
--- ratpack-site/src/ratpack/assets/config-cd8a79b7d6a80631be056d6c209395b4.rb
+++
@@ -1,8 +0,0 @@
-images_path = "assets/images"
-fonts_path = "assets/stylesheets/fonts"
-
-relative_assets = false
-# http_path = "/"
-http_images_path = "./"
-http_fonts_path = "./fonts"
-# add_import_path = "../ratpack-stylesheets"
