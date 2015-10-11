34279c1cedb88dfc68f0daf01c98245cfd658bc5
rhart <rus@gradle.com>
2015-10-11 15:39:35 +0100

    Add tck test for `IterablePromisePublisher` (#798)

---
+++ ratpack-core/src/test/groovy/ratpack/stream/tck/IterablePromisePublisherVerification.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ratpack.stream.tck
+
+import org.reactivestreams.Publisher
+import org.reactivestreams.Subscriber
+import org.reactivestreams.tck.PublisherVerification
+import org.reactivestreams.tck.TestEnvironment
+import org.testng.annotations.AfterClass
+import ratpack.exec.Promise
+import ratpack.test.exec.ExecHarness
+
+class IterablePromisePublisherVerification extends PublisherVerification<Long> {
+
+  IterablePromisePublisherVerification() {
+    super(new TestEnvironment())
+  }
+
+  private ExecHarness execHarness = ExecHarness.harness()
+
+  @Override
+  Publisher<Long> createPublisher(long elements) {
+    return new Publisher<Integer>() {
+      @Override
+      void subscribe(Subscriber<? super Integer> s) {
+        if (s == null) {
+          throw null
+        }
+
+        execHarness.fork().start {
+          def stream = Promise.value(0l..<elements).publish()
+          stream.subscribe(s)
+        }
+      }
+    }
+  }
+
+  @Override
+  Publisher<Long> createFailedPublisher() {
+    null // because subscription always succeeds. Nothing is attempted until a request is received.
+  }
+
+  @AfterClass
+  void shutdown() throws Exception {
+    execHarness.close()
+  }
+}
