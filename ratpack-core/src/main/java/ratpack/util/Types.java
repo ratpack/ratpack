d60ba83b38e9875c81d7b6ccf726ff1553503957
Luke Daley <ld@ldaley.com>
2015-10-19 08:57:54 +1000

    missing javadoc param.

--- ratpack-core/src/main/java/ratpack/util/Types.java
+++ ratpack-core/src/main/java/ratpack/util/Types.java
@@ -66,6 +66,7 @@ public abstract class Types {
    * This method should be preferred over {@link TypeToken#of(Class)} as the result may be interned when not in development.
    *
    * @param clazz the class
+   * @param <T> the type
    * @return a type token for the given class
    * @since 1.1.0
    */
