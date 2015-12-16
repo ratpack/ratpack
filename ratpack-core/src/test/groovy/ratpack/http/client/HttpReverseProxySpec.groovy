be7997f24949dcedb7f16b582c43f8521b056d5f
Luke Daley <ld@ldaley.com>
2015-12-16 12:03:29 +1000

    Add a test for a reverse proxy scenario.

---
+++ ratpack-core/src/test/groovy/ratpack/http/client/HttpReverseProxySpec.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ratpack.http.client
+
+class HttpReverseProxySpec extends HttpClientSpec {
+
+  def "can forward request body"() {
+    when:
+    otherApp {
+      post {
+        render request.body.map { "received: " + it.text }
+      }
+    }
+    handlers {
+      post { HttpClient httpClient ->
+        request.body.then { body ->
+          httpClient.requestStream(otherAppUrl()) {
+            it.method "POST"
+            it.body.buffer body.buffer
+          } then {
+            it.forwardTo(response)
+          }
+        }
+      }
+    }
+
+    then:
+    def r = request {
+      it.method "POST"
+      it.body.text "foo"
+    }
+
+    r.body.text == "received: foo"
+  }
+
+}
