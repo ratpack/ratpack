845c19363a6bf1f2cc30e51f283242eda3afae0b
Joern Huxhorn <jhuxhorn@googlemail.com>
2016-03-20 20:45:04 +0100

    Implements functionality discussed in issue #937
    - (optionally) log 4xx and 5xx at WARN level.
    - add markers corresponding to status code type.
    - use slf4j pattern to format message.

--- ratpack-core/src/test/groovy/ratpack/handling/internal/NcsaRequestLoggerSpec.groovy
+++ ratpack-core/src/test/groovy/ratpack/handling/internal/NcsaRequestLoggerSpec.groovy
@@ -20,6 +20,7 @@ import io.netty.handler.codec.http.DefaultHttpHeaders
 import io.netty.handler.codec.http.HttpMethod
 import io.netty.handler.codec.http.HttpResponseStatus
 import io.netty.handler.codec.http.HttpVersion
+import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.junit.LoggerContextRule
 import org.apache.logging.log4j.test.appender.ListAppender
 import org.apache.logging.slf4j.Log4jLogger
@@ -179,6 +180,297 @@ class NcsaRequestLoggerSpec extends Specification {
     ex.message == 'logger must not be null!'
   }
 
+
+  @Unroll
+  def '"#expectedMessage" is generated correctly using message pattern "#expectedPattern".'() {
+    setup:
+    def logger = retrieveLogger(INFO_LOGGER_NAME)
+    TimeZone.setDefault(TimeZone.getTimeZone('America/New_York'))
+
+    def requestOutcome = createRequestOutcome(host, user, timestamp, httpMethod, path, httpVersion, statusCode, responseSize, requestId)
+    def instance = new NcsaRequestLogger(logger)
+    def events = requestListAppender.getEvents()
+
+
+    when:
+    instance.log(requestOutcome)
+
+
+    then:
+    events.size() == 1
+
+    def event = events[0]
+    event.message.formattedMessage == expectedMessage
+    event.message.format == expectedPattern
+
+
+    where:
+    host        | user        | timestamp      | httpMethod | path         | httpVersion | statusCode | responseSize | requestId    | expectedMessage
+    '127.0.0.1' | null        | TEST_TIMESTAMP | 'GET'      | 'huhu'       | 'HTTP/1.1'  | 100        | -1           | null         | '127.0.0.1 - - [17/Mar/2016:17:31:33 -0400] "GET /huhu HTTP/1.1" 100 -'
+    '127.0.0.1' | null        | TEST_TIMESTAMP | 'PUT'      | 'foo'        | 'HTTP/1.1'  | 200        | -1           | null         | '127.0.0.1 - - [17/Mar/2016:17:31:33 -0400] "PUT /foo HTTP/1.1" 200 -'
+    '127.0.0.1' | null        | TEST_TIMESTAMP | 'POST'     | 'bar'        | 'HTTP/1.1'  | 300        | -1           | 'request-id' | '127.0.0.1 - - [17/Mar/2016:17:31:33 -0400] "POST /bar HTTP/1.1" 300 - id=request-id'
+    '127.0.0.1' | 'dnadams'   | TEST_TIMESTAMP | 'GET'      | 'coffee'     | 'HTTP/1.1'  | 418        | 42           | null         | '127.0.0.1 - dnadams [17/Mar/2016:17:31:33 -0400] "GET /coffee HTTP/1.1" 418 42'
+    '127.0.0.1' | 'rbradbury' | TEST_TIMESTAMP | 'GET'      | 'fahrenheit' | 'HTTP/1.1'  | 451        | -1           | null         | '127.0.0.1 - rbradbury [17/Mar/2016:17:31:33 -0400] "GET /fahrenheit HTTP/1.1" 451 -'
+    '127.0.0.1' | 'sfalken'   | TEST_TIMESTAMP | 'GET'      | 'wopr'       | 'HTTP/1.1'  | 500        | 0            | 'joshua'     | '127.0.0.1 - sfalken [17/Mar/2016:17:31:33 -0400] "GET /wopr HTTP/1.1" 500 0 id=joshua'
+
+    expectedPattern = requestId ? NcsaRequestLogger.MESSAGE_PATTERN_WITH_ID : NcsaRequestLogger.MESSAGE_PATTERN_WITHOUT_ID
+  }
+
+  @Unroll
+  def '#statusCode is logged at level "#expectedLevel" with error status codes emitted as "#logLevel"'() {
+    setup:
+    def logger = retrieveLogger(INFO_LOGGER_NAME)
+    TimeZone.setDefault(TimeZone.getTimeZone('America/New_York'))
+
+    def requestOutcome = createRequestOutcome(statusCode)
+    def instance = new NcsaRequestLogger(logger, warnLogging)
+    def events = requestListAppender.getEvents()
+
+
+    when:
+    instance.log(requestOutcome)
+
+
+    then:
+    events.size() == 1
+
+    def event = events[0]
+    event.level == expectedLevel
+
+
+    where:
+    statusCode | warnLogging
+    99         | true
+    99         | false
+    100        | true
+    100        | false
+    199        | true
+    199        | false
+    200        | true
+    200        | false
+    299        | true
+    299        | false
+    300        | true
+    300        | false
+    399        | true
+    399        | false
+    400        | true
+    400        | false
+    499        | true
+    499        | false
+    500        | true
+    500        | false
+    599        | true
+    599        | false
+    600        | true
+    600        | false
+
+    expectedLevel = resolveExpectedLevel(statusCode, warnLogging)
+    logLevel = warnLogging ? 'WARN' : 'INFO'
+  }
+
+  @Unroll
+  def 'event with status #statusCode is routed correctly using logger "#loggerName" with error status codes emitted as "#logLevel"'() {
+    setup:
+    TimeZone.setDefault(TimeZone.getTimeZone('America/New_York'))
+
+    def logger = retrieveLogger(loggerName)
+    def instance = new NcsaRequestLogger(logger, warnLogging)
+
+    def requestOutcome = createRequestOutcome(statusCode)
+
+
+    when:
+    instance.log(requestOutcome)
+
+
+    then:
+    if(inInfo) {
+      assert requestListAppender.getEvents().size() == 1
+    } else {
+      assert requestListAppender.getEvents().size() == 0
+    }
+
+    if(inServerError) {
+      assert serverErrorRequestListAppender.getEvents().size() == 1
+    } else {
+      assert serverErrorRequestListAppender.getEvents().size() == 0
+    }
+
+    if(inError) {
+      assert errorRequestListAppender.getEvents().size() == 1
+    } else {
+      assert errorRequestListAppender.getEvents().size() == 0
+    }
+
+
+    where:
+    statusCode | loggerName           | warnLogging | inInfo | inServerError | inError
+    99         | INFO_LOGGER_NAME     | true        | true   | false         | false
+    100        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    199        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    200        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    299        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    300        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    399        | INFO_LOGGER_NAME     | true        | true   | false         | false
+    400        | INFO_LOGGER_NAME     | true        | true   | false         | true
+    499        | INFO_LOGGER_NAME     | true        | true   | false         | true
+    500        | INFO_LOGGER_NAME     | true        | true   | true          | true
+    599        | INFO_LOGGER_NAME     | true        | true   | true          | true
+    600        | INFO_LOGGER_NAME     | true        | true   | false         | false
+
+    99         | WARN_LOGGER_NAME     | true        | true   | false         | false
+    100        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    199        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    200        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    299        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    300        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    399        | WARN_LOGGER_NAME     | true        | false  | false         | false
+    400        | WARN_LOGGER_NAME     | true        | true   | false         | true
+    499        | WARN_LOGGER_NAME     | true        | true   | false         | true
+    500        | WARN_LOGGER_NAME     | true        | true   | true          | true
+    599        | WARN_LOGGER_NAME     | true        | true   | true          | true
+    600        | WARN_LOGGER_NAME     | true        | true   | false         | false
+
+    99         | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    100        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    199        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    200        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    299        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    300        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    399        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    400        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    499        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+    500        | WARN_5XX_LOGGER_NAME | true        | true   | true          | true
+    599        | WARN_5XX_LOGGER_NAME | true        | true   | true          | true
+    600        | WARN_5XX_LOGGER_NAME | true        | false  | false         | false
+
+    99         | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    100        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    199        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    200        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    299        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    300        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    399        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    400        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    499        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    500        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    599        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+    600        | ERROR_LOGGER_NAME    | true        | false  | false         | false
+
+    99         | INFO_LOGGER_NAME     | false       | true   | false         | false
+    100        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    199        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    200        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    299        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    300        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    399        | INFO_LOGGER_NAME     | false       | true   | false         | false
+    400        | INFO_LOGGER_NAME     | false       | true   | false         | true
+    499        | INFO_LOGGER_NAME     | false       | true   | false         | true
+    500        | INFO_LOGGER_NAME     | false       | true   | true          | true
+    599        | INFO_LOGGER_NAME     | false       | true   | true          | true
+    600        | INFO_LOGGER_NAME     | false       | true   | false         | false
+
+    99         | WARN_LOGGER_NAME     | false       | false  | false         | false
+    100        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    199        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    200        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    299        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    300        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    399        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    400        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    499        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    500        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    599        | WARN_LOGGER_NAME     | false       | false  | false         | false
+    600        | WARN_LOGGER_NAME     | false       | false  | false         | false
+
+    99         | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    100        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    199        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    200        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    299        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    300        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    399        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    400        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    499        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    500        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    599        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+    600        | WARN_5XX_LOGGER_NAME | false       | false  | false         | false
+
+    99         | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    100        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    199        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    200        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    299        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    300        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    399        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    400        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    499        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    500        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    599        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+    600        | ERROR_LOGGER_NAME    | false       | false  | false         | false
+
+    logLevel = warnLogging ? 'WARN' : 'INFO'
+  }
+
+  @Unroll
+  def '#statusCode has the correct marker "#markerName" with error status codes emitted as "#logLevel"'() {
+    setup:
+    def logger = retrieveLogger(INFO_LOGGER_NAME)
+    TimeZone.setDefault(TimeZone.getTimeZone('America/New_York'))
+
+    def requestOutcome = createRequestOutcome(statusCode)
+    def instance = new NcsaRequestLogger(logger, warnLogging)
+    def events = requestListAppender.getEvents()
+
+
+    when:
+    instance.log(requestOutcome)
+
+
+    then:
+    events.size() == 1
+
+    def event = events[0]
+    def marker = event.marker
+    marker != null
+    marker.isInstanceOf(markerName)
+    // all status markers have ACCESS_MARKER_NAME as parent
+    marker.isInstanceOf(NcsaRequestLogger.ACCESS_MARKER_NAME)
+
+
+    where:
+    statusCode | markerName                                   | warnLogging
+    99         | NcsaRequestLogger.STATUS_UNKNOWN_MARKER_NAME | false
+    100        | NcsaRequestLogger.STATUS_1XX_MARKER_NAME     | false
+    199        | NcsaRequestLogger.STATUS_1XX_MARKER_NAME     | false
+    200        | NcsaRequestLogger.STATUS_2XX_MARKER_NAME     | false
+    299        | NcsaRequestLogger.STATUS_2XX_MARKER_NAME     | false
+    300        | NcsaRequestLogger.STATUS_3XX_MARKER_NAME     | false
+    399        | NcsaRequestLogger.STATUS_3XX_MARKER_NAME     | false
+    400        | NcsaRequestLogger.STATUS_4XX_MARKER_NAME     | false
+    499        | NcsaRequestLogger.STATUS_4XX_MARKER_NAME     | false
+    500        | NcsaRequestLogger.STATUS_5XX_MARKER_NAME     | false
+    599        | NcsaRequestLogger.STATUS_5XX_MARKER_NAME     | false
+    600        | NcsaRequestLogger.STATUS_UNKNOWN_MARKER_NAME | false
+
+    99         | NcsaRequestLogger.STATUS_UNKNOWN_MARKER_NAME | true
+    100        | NcsaRequestLogger.STATUS_1XX_MARKER_NAME     | true
+    199        | NcsaRequestLogger.STATUS_1XX_MARKER_NAME     | true
+    200        | NcsaRequestLogger.STATUS_2XX_MARKER_NAME     | true
+    299        | NcsaRequestLogger.STATUS_2XX_MARKER_NAME     | true
+    300        | NcsaRequestLogger.STATUS_3XX_MARKER_NAME     | true
+    399        | NcsaRequestLogger.STATUS_3XX_MARKER_NAME     | true
+    400        | NcsaRequestLogger.STATUS_4XX_MARKER_NAME     | true
+    499        | NcsaRequestLogger.STATUS_4XX_MARKER_NAME     | true
+    500        | NcsaRequestLogger.STATUS_5XX_MARKER_NAME     | true
+    599        | NcsaRequestLogger.STATUS_5XX_MARKER_NAME     | true
+    600        | NcsaRequestLogger.STATUS_UNKNOWN_MARKER_NAME | true
+
+    logLevel = warnLogging ? 'WARN' : 'INFO'
+
+  }
+
   // helper methods below
   private Logger retrieveLogger(String loggerName) {
     final org.apache.logging.log4j.core.Logger log4jLogger = loggerContextRule.getLogger(loggerName)
@@ -194,6 +486,20 @@ class NcsaRequestLoggerSpec extends Specification {
     requestListAppender.clear()
     serverErrorRequestListAppender.clear()
     errorRequestListAppender.clear()
+  }
+
+  private static Level resolveExpectedLevel(int statusCode, boolean warnLogging) {
+    if(!warnLogging) {
+      return Level.INFO
+    }
+    if(statusCode>=100 && statusCode<400) {
+      return Level.INFO
+    }
+    return Level.WARN
+  }
+
+  private static createRequestOutcome(int statusCode) {
+    return createRequestOutcome('127.0.0.1', 'sfalken', TEST_TIMESTAMP, 'GET', 'wopr','HTTP/1.1', statusCode, 1337, 'request-id')
   }
 
   private static RequestOutcome createRequestOutcome(String host, String user, long timestamp, String httpMethod, String path, String httpVersion, int statusCode, long responseSize, String requestId) {
