08c21077a66763a3aedfd0bd19f871c77ca9837b
Luke Daley <ld@ldaley.com>
2016-05-19 06:02:55 +1000

    Add sugar for conditionally adding to a chain.
    Closes #978.

--- ratpack-core/src/main/java/ratpack/handling/Chain.java
+++ ratpack-core/src/main/java/ratpack/handling/Chain.java
@@ -867,6 +867,72 @@ public interface Chain {
   }
 
   /**
+   * Inlines the given chain if {@code test} is {@code true}.
+   * <p>
+   * This is literally just sugar for wrapping the given action in an {@code if} statement.
+   * It can be useful when conditionally adding handlers based on state available when building the chain.
+   * <pre class="java">{@code
+   * import ratpack.test.embed.EmbeddedApp;
+   *
+   * import static org.junit.Assert.assertEquals;
+   *
+   * public class Example {
+   *
+   *   public static void main(String... args) throws Exception {
+   *     EmbeddedApp.of(a -> a
+   *       .registryOf(r -> r.add(1))
+   *       .handlers(c -> c
+   *         .when(c.getRegistry().get(Integer.class) == 0, i -> i
+   *           .get(ctx -> ctx.render("ok"))
+   *         )
+   *       )
+   *     ).test(httpClient ->
+   *       assertEquals(httpClient.get().getStatusCode(), 404)
+   *     );
+   *
+   *     EmbeddedApp.of(a -> a
+   *       .registryOf(r -> r.add(0))
+   *       .handlers(c -> c
+   *         .when(c.getRegistry().get(Integer.class) == 0, i -> i
+   *           .get(ctx -> ctx.render("ok"))
+   *         )
+   *       )
+   *     ).test(httpClient ->
+   *       assertEquals(httpClient.getText(), "ok")
+   *     );
+   *   }
+   * }
+   * }</pre>
+   *
+   * @param test whether to include the given chain action
+   * @param action the chain action to maybe include
+   * @return this
+   * @throws Exception any thrown by {@code action}
+   * @since 1.4
+   */
+  default Chain when(boolean test, Action<? super Chain> action) throws Exception {
+    if (test) {
+      action.execute(this);
+    }
+    return this;
+  }
+
+  /**
+   * Inlines the given chain if {@code test} is {@code true}.
+   * <p>
+   * Similar to {@link #when(boolean, Action)}, except obtains the action instance from the registry by the given type.
+   *
+   * @param test whether to include the given chain action
+   * @param action the chain action to maybe include
+   * @return this
+   * @throws Exception any thrown by {@code action}
+   * @since 1.4
+   */
+  default Chain when(boolean test, Class<? extends Action<? super Chain>> action) throws Exception {
+    return when(test, getRegistry().get(action));
+  }
+
+  /**
    * Invokes the given handler only if the predicate passes.
    * <p>
    * This method differs from {@link #when when()} in that it does not insert the handler;
--- ratpack-groovy/src/main/java/ratpack/groovy/handling/GroovyChain.java
+++ ratpack-groovy/src/main/java/ratpack/groovy/handling/GroovyChain.java
@@ -744,17 +744,56 @@ public interface GroovyChain extends Chain {
   }
 
   default GroovyChain when(
-          @DelegatesTo(value = GroovyContext.class, strategy = Closure.DELEGATE_FIRST) Closure<?> test,
-          Class<? extends Action<? super Chain>> action
+    @DelegatesTo(value = GroovyContext.class, strategy = Closure.DELEGATE_FIRST) Closure<?> test,
+    Class<? extends Action<? super Chain>> action
   ) throws Exception {
     return when(test, getRegistry().get(action));
   }
 
+  /**
+   * Inlines the given handlers if {@code test} is {@code true}.
+   * <p>
+   * This is literally just sugar for wrapping the given action in an {@code if} statement.
+   * It can be useful when conditionally adding handlers based on state available when building the chain.
+   *
+   * @param test whether to include the given chain action
+   * @param handlers the handlers to maybe include
+   * @return this
+   * @throws Exception any thrown by {@code action}
+   * @since 1.4
+   * @see Chain#when(boolean, Action)
+   */
+  default GroovyChain when(boolean test, @DelegatesTo(value = GroovyChain.class, strategy = Closure.DELEGATE_FIRST) Closure<?> handlers) throws Exception {
+    return when(test, chainAction(handlers));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  default GroovyChain when(boolean test, Action<? super Chain> action) throws Exception {
+    return from(Chain.super.when(test, action));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  default GroovyChain when(boolean test, Class<? extends Action<? super Chain>> action) throws Exception {
+    return from(Chain.super.when(test, action));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   default GroovyChain onlyIf(Predicate<? super Context> test, Handler handler) {
     return from(Chain.super.onlyIf(test, handler));
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   default GroovyChain onlyIf(Predicate<? super Context> test, Class<? extends Handler> handler) {
     return from(Chain.super.onlyIf(test, handler));
@@ -790,8 +829,8 @@ public interface GroovyChain extends Chain {
   }
 
   default GroovyChain onlyIf(
-          @DelegatesTo(value = GroovyContext.class, strategy = Closure.DELEGATE_FIRST) Closure<?> test,
-          Class<? extends Handler> handler
+    @DelegatesTo(value = GroovyContext.class, strategy = Closure.DELEGATE_FIRST) Closure<?> test,
+    Class<? extends Handler> handler
   ) {
     return onlyIf(test, getRegistry().get(handler));
   }
