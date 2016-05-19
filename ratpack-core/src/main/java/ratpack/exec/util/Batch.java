b947d3fd487464ac68ceb69ed13b74f76b1d4f73
Luke Daley <ld@ldaley.com>
2016-05-19 10:15:45 +1000

    Add Batch.forEach() for callback based consumption.

--- ratpack-core/src/main/java/ratpack/exec/util/Batch.java
+++ ratpack-core/src/main/java/ratpack/exec/util/Batch.java
@@ -22,9 +22,11 @@ import org.reactivestreams.Subscription;
 import ratpack.api.Nullable;
 import ratpack.exec.ExecResult;
 import ratpack.exec.Execution;
+import ratpack.exec.Operation;
 import ratpack.exec.Promise;
 import ratpack.func.Action;
 import ratpack.func.BiAction;
+import ratpack.func.BiFunction;
 import ratpack.stream.Streams;
 import ratpack.stream.TransformablePublisher;
 import ratpack.stream.internal.BufferingPublisher;
@@ -34,6 +36,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
@@ -171,11 +174,11 @@ public final class Batch<T> {
       return Promise.value(Collections.emptyList());
     }
 
-    return execInit == null ? serialYieldAll(promises) : parallelYieldAll(promises, execInit);
+    return isSerial() ? serialYieldAll(promises) : parallelYieldAll(promises, execInit);
   }
 
   /**
-   * Initiates processing of the promises, stopping at the first error.
+   * Initiates processing of the promises, stopping at the first error, returning the results when done.
    * <p>
    * This method differs from {@link #yieldAll()} in that processing will be halted as soon as the first error occurs.
    * The error will be propagated through the returned promise.
@@ -195,23 +198,107 @@ public final class Batch<T> {
       return Promise.value(Collections.emptyList());
     }
 
-    return execInit == null ? serialYield(promises) : parallelYield(promises, execInit);
+    return isSerial() ? serialYield(promises) : parallelYield(promises, execInit);
   }
 
-  private static <T> Promise<List<? extends T>> parallelYield(List<Promise<T>> promises, Action<? super Execution> execInit) {
-    List<T> results = Types.cast(promises);
-    AtomicInteger counter = new AtomicInteger(promises.size());
+  /**
+   * Initiates processing of the promises, stopping at the first error, emitting results to the given callback when available.
+   * <p>
+   * This method is useful for aggregating or reducing the batch.
+   * If processing in parallel, the consumer function may be executed concurrently.
+   * Therefore, the consumer must be thread safe.
+   * To serialise consumption of a parallel batch, use the {@link #publisher()} method.
+   * <p>
+   * The returned operation will complete after all items have been consumed or if there is an error.
+   * If processing in parallel, subsequent errors may also occur due to the promises being in-flight when the first error occurred.
+   * Such errors will be {@link Throwable#addSuppressed(Throwable)} suppressed by the first error.
+   * <p>
+   * The integer value given the to consumer indicates the source position of the corresponding promise.
+   * When processing serially, this value will increment from 0 to «size - 1».
+   * When processing in parallel, the value may not be monotonic as it reflects the source order and not completion order.
+   *
+   * <pre class="java">{@code
+   * import org.junit.Assert;
+   * import ratpack.exec.Promise;
+   * import ratpack.exec.util.Batch;
+   * import ratpack.func.Pair;
+   * import ratpack.test.exec.ExecHarness;
+   *
+   * import java.util.Arrays;
+   * import java.util.List;
+   * import java.util.Map;
+   * import java.util.concurrent.ConcurrentHashMap;
+   *
+   * public class Example {
+   *
+   *   public static void main(String... args) throws Exception {
+   *     Map<String, Integer> map = new ConcurrentHashMap<>();
+   *
+   *     ExecHarness.runSingle(e -> {
+   *       List<Promise<Pair<String, Integer>>> promises = Arrays.asList(
+   *         Promise.value(Pair.of("a", 1)),
+   *         Promise.value(Pair.of("b", 2)),
+   *         Promise.value(Pair.of("c", 3)),
+   *         Promise.value(Pair.of("d", 4))
+   *       );
+   *
+   *       Batch.of(promises)
+   *         .parallel()
+   *         .forEach((i, v) -> map.put(v.left, v.right))
+   *         .then();
+   *     });
+   *
+   *     Assert.assertEquals(Integer.valueOf(1), map.get("a"));
+   *     Assert.assertEquals(Integer.valueOf(2), map.get("b"));
+   *     Assert.assertEquals(Integer.valueOf(3), map.get("c"));
+   *     Assert.assertEquals(Integer.valueOf(4), map.get("d"));
+   *   }
+   * }
+   * }</pre>
+   *
+   * @param consumer the consumer of promise values
+   * @return an operation for the consumption of the values
+   */
+  public Operation forEach(BiAction<? super Integer, ? super T> consumer) {
+    return isSerial() ? serialForEach(promises, consumer) : parallelForEach(promises, execInit, consumer);
+  }
+
+  private boolean isSerial() {
+    return execInit == null;
+  }
+
+  private static <T> Operation serialForEach(Iterable<? extends Promise<T>> promises, BiAction<? super Integer, ? super T> consumer) {
+    return Promise.<Void>async(d ->
+      yieldPromise(promises.iterator(), 0, (i, r) -> consumer.execute(i, r.getValue()), (i, r) -> {
+        d.error(r.getThrowable());
+        return false;
+      }, () -> d.success(null))
+    ).operation();
+  }
+
+  private static <T> Operation parallelForEach(Iterable<? extends Promise<T>> promises, Action<? super Execution> execInit, BiAction<? super Integer, ? super T> consumer) {
     AtomicReference<Throwable> error = new AtomicReference<>();
+    AtomicBoolean done = new AtomicBoolean();
+    AtomicInteger wip = new AtomicInteger();
+
     return Promise.async(d -> {
-      for (int i = 0; i < promises.size(); ++i) {
-        final int finalI = i;
+      int i = 0;
+      Iterator<? extends Promise<T>> iterator = promises.iterator();
+      while (iterator.hasNext()) {
+        Promise<T> promise = iterator.next();
+        final int finalI = i++;
+        wip.incrementAndGet();
+        if (!iterator.hasNext()) {
+          done.set(true);
+        }
+
         Execution.fork()
           .onStart(execInit)
           .onComplete(e -> {
-            if (counter.decrementAndGet() == 0) {
+            if (wip.decrementAndGet() == 0 && done.get()) {
               Throwable t = error.get();
               if (t == null) {
-                d.success(results);
+                d.success(null);
               } else {
                 d.error(t);
               }
@@ -220,42 +307,33 @@ public final class Batch<T> {
           .start(e -> {
             //noinspection ThrowableResultOfMethodCallIgnored
             if (error.get() == null) {
-              promises.get(finalI).result(t -> {
+              promise.result(t -> {
                 if (t.isError()) {
                   if (!error.compareAndSet(null, t.getThrowable())) {
                     //noinspection ThrowableResultOfMethodCallIgnored
                     error.get().addSuppressed(t.getThrowable());
                   }
                 } else {
-                  results.set(finalI, t.getValue());
+                  consumer.execute(finalI, t.getValue());
                 }
               });
             }
           });
       }
-    });
+      if (i == 0) {
+        d.success(null);
+      }
+    }).operation();
   }
 
-  private static <T> Promise<List<? extends T>> serialYield(List<Promise<T>> promises) {
+  private static <T> Promise<List<? extends T>> parallelYield(List<Promise<T>> promises, Action<? super Execution> execInit) {
     List<T> results = Types.cast(promises);
-    int lastIndex = results.size() - 1;
-    return Promise.async(d ->
-      yieldPromise(promises, 0, new BiAction<Integer, ExecResult<T>>() {
-        @Override
-        public void execute(Integer i, ExecResult<T> t) throws Exception {
-          if (t.isError()) {
-            d.error(t.getThrowable());
-          } else {
-            results.set(i, t.getValue());
-            if (i < lastIndex) {
-              yieldPromise(promises, i + 1, this);
-            } else {
-              d.success(results);
-            }
-          }
-        }
-      })
-    );
+    return Promise.async(d -> parallelForEach(promises, execInit, results::set).onError(d::error).then(() -> d.success(results)));
+  }
+
+  private static <T> Promise<List<? extends T>> serialYield(List<? extends Promise<T>> promises) {
+    List<T> results = Types.cast(promises);
+    return Promise.async(d -> serialForEach(promises, results::set).onError(d::error).then(() -> d.success(results)));
   }
 
   /**
@@ -274,7 +352,7 @@ public final class Batch<T> {
    * @return the batch as a publisher
    */
   public TransformablePublisher<T> publisher() {
-    if (execInit == null) {
+    if (isSerial()) {
       return serialPublisher(promises);
     } else {
       return parallelPublisher(promises, execInit);
@@ -335,24 +413,33 @@ public final class Batch<T> {
 
   private static <T> Promise<List<? extends ExecResult<T>>> serialYieldAll(List<Promise<T>> promises) {
     List<ExecResult<T>> results = Types.cast(promises);
-    int lastIndex = results.size() - 1;
     return Promise.async(d ->
-      yieldPromise(promises, 0, new BiAction<Integer, ExecResult<T>>() {
-        @Override
-        public void execute(Integer i, ExecResult<T> t) throws Exception {
-          results.set(i, t);
-          if (i == lastIndex) {
-            d.success(results);
-          } else {
-            yieldPromise(promises, i + 1, this);
-          }
-        }
-      })
+      yieldPromise(promises.iterator(), 0,
+        results::set,
+        (i, r) -> {
+          results.set(i, r);
+          return true;
+        },
+        () -> d.success(results)
+      )
     );
   }
 
-  private static <T> void yieldPromise(List<Promise<T>> promises, int i, BiAction<Integer, ? super ExecResult<T>> withItem) {
-    promises.get(i).result(r -> withItem.execute(i, r));
+  private static <T> void yieldPromise(Iterator<? extends Promise<T>> promises, int i, BiAction<Integer, ExecResult<T>> withItem, BiFunction<Integer, ExecResult<T>, Boolean> onError, Runnable onComplete) {
+    if (promises.hasNext()) {
+      promises.next().result(r -> {
+        if (r.isError()) {
+          if (!onError.apply(i, r)) {
+            return;
+          }
+        } else {
+          withItem.execute(i, r);
+        }
+        yieldPromise(promises, i + 1, withItem, onError, onComplete);
+      });
+    } else {
+      onComplete.run();
+    }
   }
 
   private static <T> Promise<List<? extends ExecResult<T>>> parallelYieldAll(List<? extends Promise<T>> promises, Action<? super Execution> init) {
@@ -378,6 +465,5 @@ public final class Batch<T> {
       }
     });
   }
-
 
 }
