35d51e744da3034b758b551e298cce9f376173b2
Dan Woods <daniel.p.woods@gmail.com>
2016-05-19 03:33:49 -0600

    missed the impl in DefaultServerConfigBuilder

--- ratpack-core/src/main/java/ratpack/server/internal/DefaultServerConfigBuilder.java
+++ ratpack-core/src/main/java/ratpack/server/internal/DefaultServerConfigBuilder.java
@@ -302,6 +302,12 @@ public class DefaultServerConfigBuilder implements ServerConfigBuilder {
   }
 
   @Override
+  public ConfigDataBuilder object(String path, Object object) {
+    configDataBuilder.object(path, object);
+    return this;
+  }
+
+  @Override
   public ServerConfigBuilder require(String pointer, Class<?> type) {
     Class<?> previous = required.put(
       Objects.requireNonNull(pointer, "pointer cannot be null"),
