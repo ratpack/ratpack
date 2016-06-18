dac2110a6f90e968badcc1edcb792099ea6f4d72
Danny Hyun <hyun.danny+github@gmail.com>
2016-06-18 13:48:30 -0400

    Reverting Windows fix

--- ratpack-groovy/src/test/groovy/ratpack/groovy/ScriptIncludeSpec.groovy
+++ ratpack-groovy/src/test/groovy/ratpack/groovy/ScriptIncludeSpec.groovy
@@ -60,13 +60,13 @@ class ScriptIncludeSpec extends RatpackGroovyScriptAppSpec {
         }
       }
     """
-    def path = include.path.replaceAll('\\\\', '/')
+
     script """
       ratpack {
         bindings {
           bindInstance String, "foo"
         }
-        include "${path}"
+        include "${include.path}"
         handlers {
           get {
             response.send "\${get(String)}:\${get(Integer)}"
@@ -130,12 +130,10 @@ class ScriptIncludeSpec extends RatpackGroovyScriptAppSpec {
       }
     """
 
-    def nestedPath = nestedInclude.path.replaceAll('\\\\', '/')
-
     File include = getAdditionalFile("include.groovy") << """
       import static ${Groovy.name}.ratpack
       ratpack {
-        include "${nestedPath}"
+        include "${nestedInclude.path}"
         handlers {
           get("integer") {
             response.send get(Integer).toString()
@@ -144,14 +142,12 @@ class ScriptIncludeSpec extends RatpackGroovyScriptAppSpec {
       }
     """
 
-    def path = include.path.replaceAll('\\\\', '/')
-
     script """
       ratpack {
         bindings {
           bindInstance String, "foo"
         }
-        include "${path}"
+        include "${include.path}"
         handlers {
           get {
             response.send "\${get(String)}:\${get(Integer)}"
@@ -223,13 +219,13 @@ class ScriptIncludeSpec extends RatpackGroovyScriptAppSpec {
         }
       }
     """
-    def path = include.path.replaceAll('\\\\', '/')
+
     script """
       ratpack {
         bindings {
           bindInstance String, "foo"
         }
-        include "${path}"
+        include "${include.path}"
         handlers {
           get {
             response.send "main:\${get(String)}"
@@ -260,7 +256,7 @@ class ScriptIncludeSpec extends RatpackGroovyScriptAppSpec {
 
   protected File getAdditionalFile(String fileName) {
     if (!additionalFiles.containsKey(fileName)) {
-      def tokens = fileName.split('/')
+      def tokens = fileName.split(File.separator)
       if (tokens.length > 1) {
         try {
           temporaryFolder.newFolder((String[]) tokens[0..-2].toArray())
