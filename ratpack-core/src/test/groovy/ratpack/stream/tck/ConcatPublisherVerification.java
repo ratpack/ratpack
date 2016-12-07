0ef554fc925d012b107157970ff0d01007dc97f3
rhart <rus@gradle.com>
2016-12-07 11:59:08 +0000

    Fix checkstyle

--- ratpack-core/src/test/groovy/ratpack/stream/tck/ConcatPublisherVerification.java
+++ ratpack-core/src/test/groovy/ratpack/stream/tck/ConcatPublisherVerification.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.stream.tck;
 
 import org.reactivestreams.Publisher;
@@ -31,7 +47,7 @@ public class ConcatPublisherVerification extends PublisherVerification<Long> {
 
   @Override
   public Publisher<Long> createFailedPublisher() {
-    return null;// because subscription always succeeds. Nothing is attempted until a request is received.
+    return null; // because subscription always succeeds. Nothing is attempted until a request is received.
   }
 
 }
