8adeb17ebe7985a15f9b7c70a3ff938b89a6cfc2
John Engelman <john.r.engelman@gmail.com>
2018-01-01 17:07:58 -0600

    Cleanup.

--- ratpack-rx2/src/test/groovy/ratpack/rx2/flowable/RxErrorHandlingSpec.groovy
+++ ratpack-rx2/src/test/groovy/ratpack/rx2/flowable/RxErrorHandlingSpec.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.rx2.flowable
 
 import io.reactivex.BackpressureStrategy
@@ -17,7 +33,7 @@ import ratpack.groovy.test.handling.GroovyRequestFixture
 import ratpack.handling.Context
 import ratpack.handling.Handler
 import ratpack.rx2.RxRatpack
-import ratpack.rx2.internal.RatpackGroovyDslSpec
+import ratpack.test.internal.RatpackGroovyDslSpec
 
 import java.util.concurrent.Executors
 
--- ratpack-rx2/src/test/groovy/ratpack/rx2/observable/RxBlockingSpec.groovy
+++ ratpack-rx2/src/test/groovy/ratpack/rx2/observable/RxBlockingSpec.groovy
@@ -1,11 +1,27 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package ratpack.rx2.observable
 
 import io.reactivex.functions.Action
 import ratpack.error.ServerErrorHandler
 import ratpack.exec.Blocking
 import ratpack.rx2.RxRatpack
-import ratpack.rx2.internal.RatpackGroovyDslSpec
-import ratpack.rx2.internal.SimpleErrorHandler
+import ratpack.test.internal.RatpackGroovyDslSpec
+import ratpack.test.internal.SimpleErrorHandler
 
 import static ratpack.rx2.RxRatpack.observe
 import static ratpack.rx2.RxRatpack.observeEach
