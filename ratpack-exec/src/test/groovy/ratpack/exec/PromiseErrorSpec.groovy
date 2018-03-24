794b86095c87604bbc51e8ef4de2a2cc98a498f7
Tim Yates <tim@gradle.com>
2018-03-24 09:56:19 +0000

    Add predicate form to Promise.mapError

--- ratpack-exec/src/test/groovy/ratpack/exec/PromiseErrorSpec.groovy
+++ ratpack-exec/src/test/groovy/ratpack/exec/PromiseErrorSpec.groovy
@@ -201,6 +201,19 @@ class PromiseErrorSpec extends Specification {
     events == [e, "complete"]
   }
 
+  def "can map error based on predicate"() {
+    when:
+    def e = new IllegalStateException()
+    exec({
+      Promise.error(new IllegalArgumentException("!")).mapError { it.message == "!" } {
+        throw e
+      }.then(events.&add)
+    }, events.&add)
+
+    then:
+    events == [e, "complete"]
+  }
+
   def "error map does not apply when error is of different type"() {
     when:
     def e1 = new IllegalArgumentException()
