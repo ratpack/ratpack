339545436890dbe85a992b30a166c89445d81610
John Engelman <john.engelman@target.com>
2018-10-26 07:05:25 -0500

    Upgrade retrofit version and provide response in retrofit exceptions.

--- ratpack-retrofit2/src/main/java/ratpack/retrofit/RatpackRetrofitCallException.java
+++ ratpack-retrofit2/src/main/java/ratpack/retrofit/RatpackRetrofitCallException.java
@@ -16,6 +16,15 @@
 
 package ratpack.retrofit;
 
+import io.netty.buffer.Unpooled;
+import okhttp3.Request;
+import ratpack.http.Status;
+import ratpack.http.client.ReceivedResponse;
+import ratpack.http.client.internal.DefaultReceivedResponse;
+import ratpack.http.internal.ByteBufBackedTypedData;
+import ratpack.http.internal.DefaultMediaType;
+import ratpack.retrofit.internal.OkHttpHeadersBackedHeaders;
+import ratpack.util.Exceptions;
 import retrofit2.Call;
 import retrofit2.Response;
 
@@ -26,11 +35,50 @@ import java.io.IOException;
  */
 public class RatpackRetrofitCallException extends Exception {
 
+  private final Response<?> response;
+
+  /**
+   * Create a wrapped Retrofit exception
+   * @param call the Retrofit {@link Call} to wrap.
+   * @param error the error message
+   * @deprecated since 1.6.0
+   * @see #RatpackRetrofitCallException(Request, Response)
+   */
+  @Deprecated
   public RatpackRetrofitCallException(Call<?> call, String error) {
     super(call.request().url().toString() + ": " + error);
+    this.response = null;
+  }
+
+  /**
+   * Create a wrapped Retrofit exception
+   * @param request the Retrofit {@link Request} that initiated HTTP request.
+   * @param response the underlying Retrofit {@link Response} from the HTTP request.
+   * @since 1.6.0
+   */
+  public RatpackRetrofitCallException(Request request, Response<?> response) {
+    super(request.url().toString() + ": " + Exceptions.uncheck(() -> response.errorBody().string()));
+    this.response = response;
+  }
+
+  /**
+   * Get the underlying response that resulted in the exception for this HTTP request.
+   * This is useful for mapping the exception based on the response information.
+   * @return The response for the HTTP call.
+   * @since 1.6.0
+   */
+  public ReceivedResponse getResponse() {
+    return new DefaultReceivedResponse(
+      Status.of(response.code(), response.message()),
+      new OkHttpHeadersBackedHeaders(response.headers()),
+      new ByteBufBackedTypedData(
+        Unpooled.wrappedBuffer(Exceptions.uncheck(() -> response.errorBody().bytes())),
+        DefaultMediaType.get(response.errorBody().contentType() != null ? response.errorBody().contentType().toString() : null)
+      )
+    );
   }
 
   public static RatpackRetrofitCallException cause(Call<?> call, Response<?> response) throws IOException {
-    return new RatpackRetrofitCallException(call, response.errorBody().string());
+    return new RatpackRetrofitCallException(call.request(), response);
   }
 }
