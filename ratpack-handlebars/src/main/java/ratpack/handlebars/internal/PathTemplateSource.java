abb6fc9b5a13ec7bca65925341dba487e31e3565
John Engelman <john.engelman@target.com>
2018-11-29 10:36:20 -0600

    fix commons-lang3 version conflict and update method signataure from handlebars

--- ratpack-handlebars/src/main/java/ratpack/handlebars/internal/PathTemplateSource.java
+++ ratpack-handlebars/src/main/java/ratpack/handlebars/internal/PathTemplateSource.java
@@ -20,6 +20,7 @@ import com.github.jknack.handlebars.io.TemplateSource;
 import io.netty.util.CharsetUtil;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -43,8 +44,8 @@ public class PathTemplateSource implements TemplateSource {
   }
 
   @Override
-  public String content() throws IOException {
-    return new String(Files.readAllBytes(path), CharsetUtil.UTF_8);
+  public String content(Charset charset) throws IOException {
+    return new String(Files.readAllBytes(path), charset);
   }
 
   @Override
