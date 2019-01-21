1d23debe288968ee4ac1c1ab279e2104b77f1df3
John Engelman <john.engelman@target.com>
2019-01-21 12:47:33 -0600

    memory settings

--- ratpack-gradle/src/test/groovy/ratpack/gradle/functional/ContinuousBuildSpec.groovy
+++ ratpack-gradle/src/test/groovy/ratpack/gradle/functional/ContinuousBuildSpec.groovy
@@ -90,7 +90,7 @@ class ContinuousBuildSpec extends FunctionalSpec {
     }
 
     where:
-    gradleVersion << ["4.0", "4.1", "4.2", "4.3", "5.0", "5.1"]
+    gradleVersion << ["4.10.3", "5.1.1"]
   }
 
   void determinePort() {
