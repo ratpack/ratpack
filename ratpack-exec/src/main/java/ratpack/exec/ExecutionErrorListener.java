015ea431a60fd97f37b23526dd44be11f7e89bfd
Jeff Beck <beckje01@gmail.com>
2019-04-21 14:55:25 -0500

    Move ExecutionErrorListener to Exec
    Move things around and fix codenarc error

--- ratpack-exec/src/main/java/ratpack/exec/ExecutionErrorListener.java
+++ ratpack-exec/src/main/java/ratpack/exec/ExecutionErrorListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package ratpack.exec;
 
 import com.google.common.reflect.TypeToken;
-import ratpack.registry.Registry;
 
 /**
  * A handler for capturing unhandled errors in an {@link Execution}.
