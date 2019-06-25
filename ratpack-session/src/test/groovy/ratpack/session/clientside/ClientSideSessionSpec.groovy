a86c767b9b4b89ef5ee298ec7b497c9c6ff219c1
Kristin Clemens <keclemens@gmail.com>
2019-06-25 16:38:17 -0500

    Fix ClientSide decryption bug causing intermittent test failures in ClientSideSessionSpec (#1446)
    * Deserialize lastAccessToken with getLong on readLong failures
    
    * Use UTF_8 instead of ISO_8859_1 in ClientSideSessionStore
    * Remove ClientSideSessionSpec test that had been intermittently failing
    
    * Remove duplicate algorithm from test

--- ratpack-session/src/test/groovy/ratpack/session/clientside/ClientSideSessionSpec.groovy
+++ ratpack-session/src/test/groovy/ratpack/session/clientside/ClientSideSessionSpec.groovy
@@ -25,7 +25,6 @@ import ratpack.session.Session
 import ratpack.session.SessionId
 import ratpack.session.SessionModule
 import ratpack.session.SessionSpec
-import spock.lang.Issue
 
 import java.time.Duration
 
@@ -57,7 +56,7 @@ class ClientSideSessionSpec extends SessionSpec {
         return 24
       case ~/^DES.*/:
         return 8
-      default:
+      default: // also matches ~/^AES.*/
         return 16
     }
   }
@@ -344,55 +343,6 @@ class ClientSideSessionSpec extends SessionSpec {
 
     where:
     algorithm << SUPPORTED_ALGORITHMS
-  }
-
-  @Issue("This alogrithms seem to intermittently fail, so we'll be ok if they break")
-  def "can use algorithm #algorithm - ok if fails"() {
-    given:
-    modules.clear()
-    bindings {
-      module SessionModule
-      module ClientSideSessionModule, {
-        it.with {
-          int length = 16
-          switch (algorithm) {
-            case ~/^AES.*/:
-              length = 16
-              break
-            case ~/^DESede.*/:
-              length = 24
-              break
-            case ~/^DES.*/:
-              length = 8
-              break
-          }
-          secretKey = "a" * length
-          cipherAlgorithm = algorithm
-        }
-      }
-    }
-    handlers {
-      get { Session session ->
-        render session.get("value").map { it.orElse("null") }
-      }
-      post("set/:value") { Session session ->
-        render session.set("value", pathTokens.value).map { "ok" }
-      }
-    }
-
-    expect:
-    try {
-      text == "null"
-      postText("set/foo") == "ok"
-      text == "foo"
-    } catch (Exception e) {
-      e.printStackTrace()
-    }
-
-    where:
-    algorithm << [
-      "DESede/CBC/NoPadding",
-    ]
   }
 
   def "changing the signing token invalidates the session"() {
