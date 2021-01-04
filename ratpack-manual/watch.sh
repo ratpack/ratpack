ea6f37b080e1c92b57752e9f5ef3361112d1f3df
Luke Daley <ld@ldaley.com>
2021-01-04 11:26:58 +1000

    Remove some more JRuby

--- ratpack-manual/watch.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-compass watch --sass-dir src/sass --images-dir build/manual/images --javascripts-dir build/manual/js --css-dir build/manual/css -I ../ratpack-site/assets/shared-styles --relative-assets
