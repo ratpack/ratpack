80dc530582e1a35b6ebadbdf12e50a3fa4835126
John Engelman <john.r.engelman@gmail.com>
2021-02-23 12:24:55 -0600

    add specs for redirect with 100-Continue proxy

--- ratpack-core/src/test/groovy/ratpack/http/client/HttpReverseProxySpec.groovy
+++ ratpack-core/src/test/groovy/ratpack/http/client/HttpReverseProxySpec.groovy
@@ -563,6 +563,153 @@ Client error 413"""
     s.close()
   }
 
+  def "can proxy expect continue with single redirect"() {
+    given:
+    def initialRequestLatch = new CountDownLatch(1)
+    def responseLatch = new CountDownLatch(1)
+
+    when:
+    otherApp {
+      post("post") {
+        request.body.then {
+          responseLatch.countDown()
+          response.status(200).send(it.text)
+        }
+      }
+      post("redirect") {
+        redirect(307, otherAppUrl("post").toString())
+      }
+    }
+
+    handlers {
+      post {
+        def client = get(HttpClient)
+        initialRequestLatch.countDown()
+        request.body.flatMap { td ->
+          client.requestStream(otherAppUrl("redirect")) { spec ->
+            spec.method(request.method)
+            spec.headers.copy(request.headers)
+            spec.headers.set('foo', "bar")
+            spec.body { b ->
+              b.bytes(td.bytes)
+            }
+          }
+        }.then {
+          responseLatch.await()
+          it.forwardTo(response)
+        }
+      }
+    }
+    def s = withSocket {
+      write("POST / HTTP/1.1\r\n")
+      write("Expect: 100-continue\r\n")
+      write("Connection: close\r\n")
+      write("Content-Type: application/json\r\n")
+      write("Content-Length: 17\r\n")
+      write("\r\n")
+      flush()
+    }
+
+    initialRequestLatch.await()
+    def response = readStream(s.inputStream, Duration.ofSeconds(1))
+
+    then:
+    response == """HTTP/1.1 100 Continue\r\n\r\n"""
+
+    when:
+    withSocket(s) {
+      write("{\"message\": \"hi\"}\r\n")
+      flush()
+    }
+    response = s.inputStream.getText(CharsetUtil.UTF_8.name()).normalize()
+
+    then:
+    response == """HTTP/1.1 200 OK
+content-type: text/plain;charset=UTF-8
+content-length: 17
+connection: close
+
+{\"message\": \"hi\"}"""
+    cleanup:
+    s.close()
+
+  }
+
+  def "can proxy expect continue with double redirect"() {
+    given:
+    def initialRequestLatch = new CountDownLatch(1)
+    def responseLatch = new CountDownLatch(1)
+
+    when:
+    otherApp {
+      post("post") {
+        request.body.then {
+          responseLatch.countDown()
+          response.status(200).send(it.text)
+        }
+      }
+      post("redirect") {
+        redirect(307, otherAppUrl("redirect2").toString())
+      }
+      post("redirect2") {
+        redirect(307, otherAppUrl("post").toString())
+      }
+    }
+
+    handlers {
+      post {
+        def client = get(HttpClient)
+        initialRequestLatch.countDown()
+        request.body.flatMap { td ->
+          client.requestStream(otherAppUrl("redirect")) { spec ->
+            spec.method(request.method)
+            spec.headers.copy(request.headers)
+            spec.headers.set('foo', "bar")
+            spec.body { b ->
+              b.bytes(td.bytes)
+            }
+          }
+        }.then {
+          responseLatch.await()
+          it.forwardTo(response)
+        }
+      }
+    }
+    def s = withSocket {
+      write("POST / HTTP/1.1\r\n")
+      write("Expect: 100-continue\r\n")
+      write("Connection: close\r\n")
+      write("Content-Type: application/json\r\n")
+      write("Content-Length: 17\r\n")
+      write("\r\n")
+      flush()
+    }
+
+    initialRequestLatch.await()
+    def response = readStream(s.inputStream, Duration.ofSeconds(1))
+
+    then:
+    response == """HTTP/1.1 100 Continue\r\n\r\n"""
+
+    when:
+    withSocket(s) {
+      write("{\"message\": \"hi\"}\r\n")
+      flush()
+    }
+    response = s.inputStream.getText(CharsetUtil.UTF_8.name()).normalize()
+
+    then:
+    response == """HTTP/1.1 200 OK
+content-type: text/plain;charset=UTF-8
+content-length: 17
+connection: close
+
+{\"message\": \"hi\"}"""
+    cleanup:
+    s.close()
+
+  }
+
   static List<HttpResponseStatus> noBodyResponseStatuses() {
     [HttpResponseStatus.valueOf(100), HttpResponseStatus.valueOf(150), HttpResponseStatus.valueOf(199), HttpResponseStatus.valueOf(204), HttpResponseStatus.valueOf(304)]
   }
