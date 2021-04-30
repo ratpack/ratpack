867cb44e98a3c145576e4477bef16fa452d9b8e8
Luke Daley <ld@ldaley.com>
2021-04-30 15:35:04 +1000

    Add slightly better documentation

--- ratpack-session-serialization-kryo/src/main/java/ratpack/session/serialization/kryo/KryoSessionSerializer.java
+++ ratpack-session-serialization-kryo/src/main/java/ratpack/session/serialization/kryo/KryoSessionSerializer.java
@@ -27,10 +27,21 @@ import com.esotericsoftware.kryo.kryo5.util.DefaultClassResolver;
 import com.google.inject.Singleton;
 import io.netty.util.concurrent.FastThreadLocal;
 import ratpack.session.JavaSessionSerializer;
+import ratpack.session.SessionModule;
 import ratpack.session.SessionTypeFilter;
 
 import java.io.*;
 
+/**
+ * A <a href="https://github.com/EsotericSoftware/kryo">Kryo</a> based session data serialization implementation.
+ * <p>
+ * For use in combination with {@link SessionModule}.
+ * To use, override the {@link JavaSessionSerializer} binding provided by that module with an instance of this class.
+ *
+ * This serializer supports session type filtering via {@link SessionTypeFilter}.
+ *
+ * @since 1.9
+ */
 @Singleton
 public class KryoSessionSerializer implements JavaSessionSerializer {
 
@@ -90,6 +101,15 @@ public class KryoSessionSerializer implements JavaSessionSerializer {
     return kryo;
   }
 
+  /**
+   * A hook for potential subclasses to configure Kryo instances used.
+   * <p>
+   * Internally, kryo instances are pooled and reused.
+   * This method may be called any time, as new instances are needed.
+   * All instances should be configured identically.
+   *
+   * @param kryo the instance to configure
+   */
   protected void configureKryo(@SuppressWarnings("unused") Kryo kryo) {
 
   }
