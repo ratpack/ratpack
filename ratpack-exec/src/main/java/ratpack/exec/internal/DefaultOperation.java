53bfdffef2f91cffa3ab9fab5a2c067cb434b7e8
Luke Daley <ld@ldaley.com>
2022-06-15 19:52:42 +1000

    Fix operation implementation to properly propagate upstream signals

--- ratpack-exec/src/main/java/ratpack/exec/internal/DefaultOperation.java
+++ ratpack-exec/src/main/java/ratpack/exec/internal/DefaultOperation.java
@@ -16,35 +16,138 @@
 
 package ratpack.exec.internal;
 
-import ratpack.exec.Operation;
-import ratpack.exec.Promise;
+import ratpack.exec.*;
 import ratpack.func.Action;
 import ratpack.func.Block;
 import ratpack.func.Predicate;
 
 public class DefaultOperation implements Operation {
 
-  private final Promise<Void> promise;
+  private final Upstream<?> upstream;
 
-  public DefaultOperation(Promise<Void> promise) {
-    this.promise = promise;
+  public DefaultOperation(Upstream<?> upstream) {
+    this.upstream = upstream;
+  }
+
+  public DefaultOperation(Promise<?> promise) {
+    this(downstream -> promise.connect(new Downstream<Object>() {
+      @Override
+      public void success(Object value) {
+        downstream.success(null);
+      }
+
+      @Override
+      public void error(Throwable throwable) {
+        downstream.error(throwable);
+      }
+
+      @Override
+      public void complete() {
+        downstream.complete();
+      }
+    }));
   }
 
   @Override
   public Promise<Void> promise() {
-    return promise;
+    return Promise.async(down ->
+      upstream.connect(new Downstream<Object>() {
+        @Override
+        public void success(Object value) {
+          down.success(null);
+        }
+
+        @Override
+        public void error(Throwable throwable) {
+          down.error(throwable);
+        }
+
+        @Override
+        public void complete() {
+          down.complete();
+        }
+      })
+    );
   }
 
   @Override
   public Operation onError(Predicate<? super Throwable> predicate, Action<? super Throwable> errorHandler) {
-    return new DefaultOperation(promise.onError(predicate, errorHandler));
+    return new DefaultOperation(down ->
+      upstream.connect(new Downstream<Object>() {
+        @Override
+        public void success(Object value) {
+          down.success(null);
+        }
+
+        @SuppressWarnings("DuplicatedCode")
+        @Override
+        public void error(Throwable throwable) {
+          try {
+            if (predicate.apply(throwable)) {
+              Promise.<Void>sync(() -> {
+                  errorHandler.execute(throwable);
+                  return null;
+                })
+                .connect(new Downstream<Void>() {
+                  @Override
+                  public void success(Void value) {
+                    down.complete();
+                  }
+
+                  @Override
+                  public void error(Throwable e) {
+                    down.error(e);
+                  }
+
+                  @Override
+                  public void complete() {
+                    down.complete();
+                  }
+                });
+            } else {
+              down.error(throwable);
+            }
+          } catch (Throwable e) {
+            down.error(e);
+          }
+        }
+
+        @Override
+        public void complete() {
+          down.complete();
+        }
+      })
+    );
   }
 
   @Override
   public void then(Block block) {
-    promise.then(v ->
-      block.execute()
-    );
+    try {
+      upstream.connect(new Downstream<Object>() {
+        @Override
+        public void success(Object value) {
+          try {
+            block.execute();
+          } catch (Throwable e) {
+            DefaultPromise.throwError(e);
+          }
+        }
+
+        @Override
+        public void error(Throwable throwable) {
+          DefaultPromise.throwError(throwable);
+        }
+
+        @Override
+        public void complete() {
+
+        }
+      });
+    } catch (ExecutionException e) {
+      throw e;
+    } catch (Throwable e) {
+      DefaultPromise.throwError(e);
+    }
   }
 
 }
