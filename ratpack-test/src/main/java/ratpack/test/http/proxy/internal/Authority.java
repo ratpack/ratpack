acb44379d26f64aa90d0522515afa771303f932f
Jim Hurne <jhurne@gradle.com>
2022-07-21 19:58:36 -0400

    Fix more code style issues

--- ratpack-test/src/main/java/ratpack/test/http/proxy/internal/Authority.java
+++ ratpack-test/src/main/java/ratpack/test/http/proxy/internal/Authority.java
@@ -47,8 +47,12 @@ public class Authority {
 
   @Override
   public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
     Authority authority = (Authority) o;
     return port == authority.port && host.equals(authority.host);
   }
